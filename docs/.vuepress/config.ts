f1b0436014a900c1978be78a704f9f3dc56053c2
Alin Eugen Deac <aedart@gmail.com>
2023-03-19 18:06:33 +0100

    Fix double slashes

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -15,10 +15,10 @@ export default defineUserConfig({
 
     head: [
         // Icon
-        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: resolvePath('/images/icon/apple-touch-icon.png') }],
-        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: resolvePath('/images/icon/favicon-32x32.png') }],
-        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: resolvePath('/images/icon/favicon-16x16.png') }],
-        ['link', { rel: 'manifest', href: resolvePath('/site.webmanifest') }],
+        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: resolvePath('images/icon/apple-touch-icon.png') }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: resolvePath('images/icon/favicon-32x32.png') }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: resolvePath('images/icon/favicon-16x16.png') }],
+        ['link', { rel: 'manifest', href: resolvePath('site.webmanifest') }],
     ],
 
     theme: defaultTheme({
