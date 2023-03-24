7d72d670471c352e4bf197c2ac5cb13eff4a5e74
Alin Eugen Deac <aedart@gmail.com>
2023-03-24 19:37:38 +0100

    Change export, deal with vue components  "manually"
    Instead of attempting to render, transpile... or else-what, the components are now just copied manually into "dist/components".

--- packages/vuepress-utils/rollup.config.mjs
+++ packages/vuepress-utils/rollup.config.mjs
@@ -2,4 +2,12 @@ import { createConfig } from '../../shared/rollup.config.mjs';
 
 export default createConfig({
     baseDir: new URL('.', import.meta.url),
+    
+    // Because this package offers *.vue files, we need to
+    // be explicit about which to export! 
+    submodules: [
+        //'components', // don't!
+        'contracts',
+        'navigation'
+    ]
 });
