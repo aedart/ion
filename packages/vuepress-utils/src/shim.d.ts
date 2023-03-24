6c212e41befa7707a3b82484f0cf4e5861c2fd4e
Alin Eugen Deac <aedart@gmail.com>
2023-03-24 19:34:46 +0100

    Change shim to handle "define component" from vue

--- packages/vuepress-utils/src/shim.d.ts
+++ packages/vuepress-utils/src/shim.d.ts
@@ -1,7 +1,9 @@
 /**
+ * @see https://www.thisdot.co/blog/how-to-create-and-deploy-a-vue-component-library-to-npm
  * @see https://fettblog.eu/typescript-modules-for-webpack/
  */
-declare module "*.vue" {
-    import Vue from 'vue'
-    export default Vue
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue';
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
 }
