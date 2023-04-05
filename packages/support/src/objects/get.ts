0ff02c7cfc22fd6fe925094cd52c8cb4ad5db295
Alin Eugen Deac <aedart@gmail.com>
2023-04-05 21:31:49 +0200

    Cleanup

--- packages/support/src/objects/get.ts
+++ packages/support/src/objects/get.ts
@@ -1,9 +1,4 @@
 import {get as _get} from 'lodash-es';
-// import type {
-//     PropertyPath,
-//     NumericDictionary,
-//     GetFieldType
-// } from 'lodash';
 
 /**
  * Get value from object that matches given path
