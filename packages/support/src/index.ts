eff544724cb4f20921e03333f6fd0fc5198ecc20
Alin Eugen Deac <aedart@gmail.com>
2023-04-05 21:10:45 +0200

    Add get util method

--- packages/support/src/index.ts
+++ packages/support/src/index.ts
@@ -2,6 +2,7 @@
  * Utility Methods
  ****************************************************************/
 
+export * from './utils/get';
 export * from './utils/has';
 export * from './utils/hasAll';
 export * from './utils/hasAny';
