6451460b646693fc2fd214637bb0cd03550300f1
Alin Eugen Deac <aedart@gmail.com>
2023-04-05 21:25:03 +0200

    Remove property key type
    This should no longer be needed

--- packages/contracts/src/types.ts
+++ packages/contracts/src/types.ts
@@ -12,10 +12,3 @@ export type AbstractConstructor<T> = abstract new (...args: any[]) => T;
  * Constructor or Abstract Constructor type
  */
 export type ConstructorOrAbstractConstructor<T> = Constructor<T> | AbstractConstructor<T>;
-
-/**
- * TODO: This should perhaps belong elsewhere...
- * 
- * Object property key
- */
-export type PropertyKey = string | number | symbol;
