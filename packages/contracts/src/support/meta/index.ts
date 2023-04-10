f61d55824d20739f5f04a0e05bdcc6d5d17cacde
Alin Eugen Deac <aedart@gmail.com>
2023-04-10 12:42:03 +0200

    Export Meta Registry

--- packages/contracts/src/support/meta/index.ts
+++ packages/contracts/src/support/meta/index.ts
@@ -1,6 +1,14 @@
+import Registry, { REGISTRY } from "./Registry";
+
 /**
  * Support Meta identifier
  *
  * @type {Symbol}
  */
 export const SUPPORT_META: unique symbol = Symbol('@aedart/contracts/support/meta');
+
+//export type * from './types';
+export {
+    type Registry,
+    REGISTRY
+};
