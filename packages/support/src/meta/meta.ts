bff66254f96d5b65b8c7b97d9e77df5a862ff738
Alin Eugen Deac <aedart@gmail.com>
2023-04-14 17:55:39 +0200

    Change decorator, accept callback as key argument
    When a callback is given, more advanced key-value pair can be specified by developer. This allows creating metadata is somehow is dependent on target or the decorator context.

--- packages/support/src/meta/meta.ts
+++ packages/support/src/meta/meta.ts
@@ -1,34 +1,72 @@
 import type {Key} from "@aedart/contracts/support";
-import type { Context } from "@aedart/contracts/support/meta";
+import type { Context, MetaCallback, MetaEntry } from "@aedart/contracts/support/meta";
 import { set } from "@aedart/support/objects";
 
-// TODO: This acts as a "fallback" metadata registry, when context.metadata is not available!
-const registry: WeakMap<object, Record<string | number | symbol, unknown>> = new WeakMap<object, Record<string | number | symbol, unknown>>()
+/**
+ * Fallback registry of metadata, in case that `context.metadata` is
+ * not available.
+ * 
+ * This registry is NOT intended to be available for writing,
+ * outside the scope of the meta decorator.
+ * 
+ * @type {WeakMap<object, Record<string | number | symbol, unknown>>}
+ */
+const registry: WeakMap<object, Record<string | number | symbol, unknown>> = new WeakMap<object, Record<string | number | symbol, unknown>>();
 
 // TODO: The metadata symbol... we will need this when reading metadata from a target.
 // const metadataSymbol = Symbol.for('metadata');
 
-// TODO
+/**
+ * Store value as metadata, for given key
+ * 
+ * @param {Key | MetaCallback} key Key or path identifier. If callback is given,
+ *                                 then its resulting {@link MetaEntry}'s `key`
+ *                                 and `value` are stored.
+ * @param {unknown} [value] Value to store. Ignored if `key` argument is
+ *                          a callback.
+ * 
+ * @returns {(target: object, context: Context) => void}
+ */
 export function meta(
-    key: Key,
-    value: unknown
+    key: Key | MetaCallback,
+    value?: unknown
 ) {
     return (
         target: object,
         context: Context
     ) => {
-        // TODO: target is "undefined" for context.kind === 'field'
-        // TODO: @see https://github.com/tc39/proposal-decorators#class-fields
+        // Resolve the metadata object, either from registry or decorator context.
+        let metadata: Record<string | number | symbol, unknown> = registry.get(target) ?? {}; /* eslint-disable-line prefer-const */
+        let useFallback: boolean = true;
+        const isMetaCallback: boolean = (typeof key === 'function');
 
-        // Use default context.metadata, if it's available.
         if (Reflect.has(context, 'metadata') && typeof context.metadata === 'object') {
-            set(context.metadata, key, value);
-            return;
+            metadata = context.metadata;
+            useFallback = false;
         }
         
-        // Fallback, use custom "registry" for given target.
-        let metadata: Record<string | number | symbol, unknown> = registry.get(target) ?? {}; /* eslint-disable-line prefer-const */
-        set(metadata, key, value);
-        registry.set(target, metadata);
+        // Unless the key is a callback, then we need to abort if target is undefined, or
+        // a class field is attempted decorated. Default behaviour is not able to store
+        // metadata for an "undefined" target.
+        // @see https://github.com/tc39/proposal-decorators#class-fields
+        if (!isMetaCallback && (target === undefined || context.kind === 'field')) {
+            throw new TypeError('Unable to store metadata for class field, or undefined target.');
+        }
+
+        // In case a callback is given as key, resolve it. The resulting meta entry object's
+        // key-value are then used instead.
+        if (isMetaCallback) {
+            const entry = (key as MetaCallback)(target, context);
+            key = entry.key;
+            value = entry.value;
+        }
+
+        // Set the metadata value.
+        set(metadata, (key as Key), value);
+        
+        // Save metadata object in registry, when fallback approach is used.
+        if (useFallback) {
+            registry.set(target, metadata);    
+        }
     }
 }
