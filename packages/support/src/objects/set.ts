5663f7f9c6d64ece648145ccb98b3d1349124b6f
Alin Eugen Deac <aedart@gmail.com>
2023-04-14 17:53:56 +0200

    Fix JSDoc typedef
    These should not had been declared in function's JSDoc.

--- packages/support/src/objects/set.ts
+++ packages/support/src/objects/set.ts
@@ -1,10 +1,12 @@
 import { set as _set } from 'lodash-es';
 
 /**
+ * @typedef {import('@aedart/contracts/support').Key} Key
+ */
+
+/**
  * Set value in object at given path
  * (Alias for Lodash' {@link import('lodash').set set}) method
- *
- * @typedef {import('@aedart/contracts/support').Key} Key
  * 
  * @type {{<T extends object>(object: T, path: Key, value: any): T, <TResult>(object: object, path: Key, value: any): TResult}}
  */
