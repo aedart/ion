59e1f057b1fabf99c26faf1364a6c27d1e716688
Alin Eugen Deac <aedart@gmail.com>
2023-04-21 19:08:06 +0200

    Add meta decorator tests for class getter methods (incomplete)

---
+++ tests/browser/packages/support/meta/getter.test.js
@@ -0,0 +1,127 @@
+import {
+    meta,
+    getMeta
+} from '@aedart/support/meta';
+
+describe('@aedart/support/meta', () => {
+
+    describe('getter', () => {
+
+        it('can set and get meta on getter', () => {
+
+            const key = 'fip'
+            const value = 'fup';
+
+            class A {
+
+                @meta(key, value)
+                get foo() {
+                    return 'bar'
+                }
+            }
+
+            // instance required... 
+            new A();
+            
+            // ----------------------------------------------------------------- //
+            
+            expect(getMeta(A, key))
+                .withContext('Incorrect meta value obtained')
+                .toBe(value)
+        });
+
+        it('can set and get meta on private getter', () => {
+
+            const key = 'bar'
+            const value = 'sweet';
+
+            class A {
+
+                @meta(key, value)
+                get #foo() {
+                    return 'bar'
+                }
+            }
+
+            // instance required... 
+            new A();
+
+            // ----------------------------------------------------------------- //
+
+            expect(getMeta(A, key))
+                .withContext('Incorrect meta value obtained')
+                .toBe(value)
+        });
+
+        it('can set and get meta on static getter', () => {
+
+            const key = 'bar'
+            const value = 9874;
+
+            class A {
+
+                @meta(key, value)
+                static get foo() {
+                    return 'bar';
+                }
+            }
+
+            // ----------------------------------------------------------------- //
+
+            expect(getMeta(A, key))
+                .withContext('Incorrect meta value obtained')
+                .toBe(value);
+        });
+
+        it('can set and get meta on static private getter', () => {
+
+            const key = 'bar'
+            const value = 'zip';
+
+            class A {
+
+                @meta(key, value)
+                static get #foo() {
+                    return 'bar';
+                }
+            }
+
+            // ----------------------------------------------------------------- //
+
+            expect(getMeta(A, key))
+                .withContext('Incorrect meta value obtained')
+                .toBe(value);
+        });
+
+        // TODO: ...
+        // it('runs additional initialize callback for getter', () => {
+        //
+        //     const key = 'foo';
+        //     const value = 'bar';
+        //     let invoked = false;
+        //
+        //     @meta((target, context) => {
+        //         context.addInitializer(() => {
+        //             invoked = true;
+        //         });
+        //
+        //         return {
+        //             key: key,
+        //             value: value
+        //         }
+        //     })
+        //     class A {}
+        //
+        //     // instance required... 
+        //     new A();
+        //
+        //     // ----------------------------------------------------------------- //
+        //
+        //     // console.log('was invoked', invoked)
+        //
+        //     expect(invoked)
+        //         .withContext('Init callback not invoked for method')
+        //         .toBeTrue();
+        // });
+    });
+});
