1564aeaadc0e8333d313d67c9c786c51c9c6bca8
Alin Eugen Deac <aedart@gmail.com>
2023-04-24 19:04:04 +0200

    Add Reflection interface

---
+++ packages/contracts/src/support/reflections/Reflection.ts
@@ -0,0 +1,53 @@
+import { Arrayable } from "@aedart/contracts/support";
+
+/**
+ * Reflection
+ */
+export default interface Reflection extends Arrayable
+{
+    /**
+     * The name of the element
+     * 
+     * @returns {string | symbol}
+     */
+    get name(): string | symbol;
+    
+    /**
+     * The kind of element
+     * 
+     * @returns {string}
+     */
+    get kind(): string;
+
+    /**
+     * Determine if element is declared as static
+     * 
+     * @returns {boolean}
+     */
+    get static(): boolean;
+
+    /**
+     * Determine if element is declared as private
+     * 
+     * @returns {boolean}
+     */
+    get private(): boolean;
+
+    /**
+     * Opposite of {@link private}
+     * 
+     * @returns {boolean}
+     */
+    get public(): boolean;
+
+    /**
+     * Reference to target element
+     * 
+     * **Note**: value is undefined if target element is not an
+     * object (e.g. if a primitive value), or if target element
+     * otherwise cannot be referenced.
+     * 
+     * @returns {WeakRef<object> | undefined}
+     */
+    get target(): WeakRef<object> | undefined;
+}
--- packages/contracts/src/support/reflections/index.ts
+++ packages/contracts/src/support/reflections/index.ts
@@ -1,1 +1,12 @@
-export const TODO: string = 'TODO';
+import Reflection from "./Reflection";
+
+/**
+ * Support Reflections identifier
+ *
+ * @type {Symbol}
+ */
+export const SUPPORT_REFLECTIONS: unique symbol = Symbol('@aedart/contracts/support/reflections');
+
+export {
+    type Reflection
+}
