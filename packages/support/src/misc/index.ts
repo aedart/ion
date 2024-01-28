6bf48f42f371a18a9b9cef805271b8054186c62e
Alin Eugen Deac <aedart@gmail.com>
2024-01-28 15:17:12 +0100

    Add isPropertyKey util method

--- packages/support/src/misc/index.ts
+++ packages/support/src/misc/index.ts
@@ -1,5 +1,6 @@
 export * from './empty';
 export * from './isPrimitive';
+export * from './isPropertyKey';
 export * from './isset';
 export * from './descTag';
 export * from './toWeakRef'
---
+++ packages/support/src/misc/isPropertyKey.ts
@@ -0,0 +1,13 @@
+/**
+ * Determine if given key is a valid property key name
+ * 
+ * @see {PropertyKey}
+ * 
+ * @param {any} key
+ * 
+ * @returns {boolean} True if typeof key is a string, number or symbol
+ */
+export function isPropertyKey(key: any): boolean
+{
+    return [ 'string', 'number', 'symbol' ].includes(typeof key);
+}
