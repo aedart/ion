d34188963e5fa80f6356c91db6b5d13e8bfce5c2
Alin Eugen Deac <aedart@gmail.com>
2024-01-28 14:54:01 +0100

    Remove PropertyKey
    TypeScript has it's own definition hereof. It is therefore no longer required.

--- packages/contracts/src/support/types.ts
+++ packages/contracts/src/support/types.ts
@@ -4,11 +4,6 @@
 export type Key = OneOrMany<PropertyKey>;
 
 /**
- * Object Property key
- */
-export type PropertyKey = string | number | symbol;
-
-/**
  * One or many
  */
 export type OneOrMany<T> = T | ReadonlyArray<T>;
