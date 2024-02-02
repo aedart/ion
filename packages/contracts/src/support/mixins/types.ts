c2b501d9202a11a83d7e783fa57da5e842ee9b6a
Alin Eugen Deac <aedart@gmail.com>
2024-02-02 20:07:15 +0100

    Ignore explicit any

--- packages/contracts/src/support/mixins/types.ts
+++ packages/contracts/src/support/mixins/types.ts
@@ -2,6 +2,6 @@ import type { ConstructorOrAbstractConstructor } from "@aedart/contracts";
 
 // TODO: Describe this type...
 export type ClassDecorator<
-    T extends ConstructorOrAbstractConstructor<any> = any,
-    U extends ConstructorOrAbstractConstructor<any> = any,
+    T extends ConstructorOrAbstractConstructor<any> = any, /* eslint-disable-line @typescript-eslint/no-explicit-any */
+    U extends ConstructorOrAbstractConstructor<any> = any, /* eslint-disable-line @typescript-eslint/no-explicit-any */
 > = (superclass: T) => ConstructorOrAbstractConstructor<U>;
