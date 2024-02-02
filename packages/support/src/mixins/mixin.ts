9c772387b2fe94ba2b43f9cb02ccd6c3e10bdb56
Alin Eugen Deac <aedart@gmail.com>
2024-02-02 20:20:34 +0100

    Add mixin logic
    This is the first draft implementation. Unit testing can now begin...

--- packages/support/src/mixins/mixin.ts
+++ packages/support/src/mixins/mixin.ts
@@ -1,5 +1,34 @@
+import type { ConstructorOrAbstractConstructor } from "@aedart/contracts";
+import type { ClassDecorator } from "@aedart/contracts/support/mixins";
+
 // TODO: ...
-export function mixin()
+export function mixin(...mixins: ClassDecorator[])
 {
+    // Fail if no mixins are provided.
+    if (arguments.length == 0) {
+        throw new TypeError(`@mixin() must be given at least one class decorator (mixin) as argument`);
+    }
     
+    return (target: object, context: DecoratorContext) => {
+        // Fail if target is not a class
+        if (context.kind !== 'class') {
+            throw new TypeError(`@mixin() can only by applied on classes - "${context.kind}" is not support`);
+        }
+        
+        return mixins.reduce((
+            superclass: ConstructorOrAbstractConstructor<any>, /* eslint-disable-line @typescript-eslint/no-explicit-any */
+            mixin: ClassDecorator
+        ) => {
+            // Return superclass, when mixin isn't a function.
+            if (typeof mixin != 'function') {
+                return superclass;
+            }
+            
+            // TODO: Prevent mixin, if already mixed into the superclass!
+                // TODO: What about the "instance of" checks?
+            
+            // Perform actual mixin and return resulting class...
+            return mixin(superclass);
+        }, target);
+    };
 }
