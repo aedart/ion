0bdb6f4c384ec891da88c2e297a1874cdf638bb4
Alin Eugen Deac <aedart@gmail.com>
2024-02-06 20:20:28 +0100

    Fix Symbol.hasInstance not added on mixin

--- packages/support/src/mixins/mixins/HasInstance.ts
+++ packages/support/src/mixins/mixins/HasInstance.ts
@@ -12,14 +12,16 @@ export const HasInstance = function(mixin: Mixin): Mixin
 {
     // The following source code is an adaptation of Justin Fagnani's "mixwith.js" (Apache License 2.0)
     // @see https://github.com/justinfagnani/mixwith.js
-    
-    if (!Reflect.has(mixin, Symbol.hasInstance)) {
-        Reflect.defineProperty(mixin, Symbol.hasInstance, {
-            value(instance) {
-                return hasMixin(instance, mixin);
-            },
-        })
+
+    // Skip if mixin already has Symbol.hasInstance
+    if (Object.hasOwn(mixin, Symbol.hasInstance)) {
+        return mixin;
     }
     
-    return mixin;
+    // Otherwise, define the Symbol.hasInstance
+    return Object.defineProperty(mixin, Symbol.hasInstance, {
+        value: (instance: object) => {
+            return hasMixin(instance, mixin);
+        },
+    });
 }
