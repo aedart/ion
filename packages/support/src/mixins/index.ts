3397fa5e75a171247e3b82ebae0d7b7487d1e39a
Alin Eugen Deac <aedart@gmail.com>
2024-02-06 19:43:53 +0100

    Export decorators and other utils

--- packages/support/src/mixins/index.ts
+++ packages/support/src/mixins/index.ts
@@ -1,1 +1,7 @@
+export * from './mixins';
+export * from './apply';
+export * from './hasMixin';
+export * from './isApplicationOf';
 export * from './mix';
+export * from './unwrap';
+export * from './wrap';
