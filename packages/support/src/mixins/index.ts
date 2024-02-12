319975bcc3317c0fcd89e2d4ce0e363acc09c756
alin <alin@rspsystems.com>
2024-02-12 13:15:41 +0100

    Remove deprecated mix() class decorator
    Has now been replaced with the original mix() / Mixin Builder component.

--- packages/support/src/mixins/index.ts
+++ packages/support/src/mixins/index.ts
@@ -7,7 +7,6 @@ export * from './decorators';
 export * from './apply';
 export * from './hasMixin';
 export * from './isApplicationOf';
-export * from './_mix';
 export * from './mix';
 export * from './unwrap';
 export * from './wrap';
