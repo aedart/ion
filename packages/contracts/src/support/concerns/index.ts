104b6df401737934097c7cdd468cd4aa4e82764f
Alin Eugen Deac <aedart@gmail.com>
2024-02-13 20:36:32 +0100

    Export Concern interface

--- packages/contracts/src/support/concerns/index.ts
+++ packages/contracts/src/support/concerns/index.ts
@@ -1,6 +1,12 @@
+import Concern from "./Concern";
+
 /**
  * Support Concerns identifier
  *
  * @type {Symbol}
  */
 export const SUPPORT_CONCERNS: unique symbol = Symbol('@aedart/contracts/support/concerns');
+
+export {
+    type Concern
+}
