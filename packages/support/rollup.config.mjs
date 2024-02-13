abfb916496a8b6461984917caf03fb186e1d561e
Alin Eugen Deac <aedart@gmail.com>
2024-02-13 20:31:52 +0100

    Add support/concerns submodule

--- packages/support/rollup.config.mjs
+++ packages/support/rollup.config.mjs
@@ -4,6 +4,7 @@ export default createConfig({
     baseDir: new URL('.', import.meta.url),
     external: [
         '@aedart/contracts/support',
+        '@aedart/contracts/support/concerns',
         '@aedart/contracts/support/meta',
         '@aedart/contracts/support/mixins',
         '@aedart/contracts/support/reflections',
