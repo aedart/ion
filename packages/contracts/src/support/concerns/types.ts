e253f6cbca9c67e9988fddfbb88fba7c83f8a9f1
alin <alin@rspsystems.com>
2024-02-14 12:46:06 +0100

    Improve description of aliases object

--- packages/contracts/src/support/concerns/types.ts
+++ packages/contracts/src/support/concerns/types.ts
@@ -2,6 +2,10 @@ import Concern from "./Concern";
 
 /**
  * A record that defines one or more aliases for a {@link Concern}'s properties or methods.
+ * 
+ * In this context an "alias" means a property or method name that is added onto a target
+ * class' prototype and acts as a proxy to the original property or method inside the
+ * concern class.
  */
 export type Aliases<T extends Concern> = {
     [P in keyof T]: PropertyKey
