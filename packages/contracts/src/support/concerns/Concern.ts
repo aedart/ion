404cf5700d3d2d5c3dcdd01a902d43ace8555530
alin <alin@rspsystems.com>
2024-02-14 11:06:12 +0100

    Fix typo

--- packages/contracts/src/support/concerns/Concern.ts
+++ packages/contracts/src/support/concerns/Concern.ts
@@ -11,7 +11,7 @@ export default interface Concern
     /**
      * Creates a new concern instance
      * 
-     * @param {object} owner The target class instance this concern was injected into
+     * @param {object} owner The target class instance this concern is injected into
      * 
      * @throws {Error} When concern is unable to preform initialisation, e.g. caused
      *                 by the owner or other circumstances. 
@@ -19,7 +19,7 @@ export default interface Concern
     constructor(owner: object);
     
     /**
-     * Returns the target class instance this concern was injected into
+     * Returns the target class instance this concern is injected into
      * 
      * @return {object}
      */
