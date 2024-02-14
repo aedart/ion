bc45baf40c665b59bb2d0e3ccc778e10c015f4f3
alin <alin@rspsystems.com>
2024-02-14 10:32:51 +0100

    Add constructor and get concern owner
    A concern should at the very minimum be able to obtain the target class instance that it was injected into.

--- packages/contracts/src/support/concerns/Concern.ts
+++ packages/contracts/src/support/concerns/Concern.ts
@@ -1,7 +1,21 @@
 /**
  * Concern
+ * 
+ * A component that can be injected into a target class (concern owner).
  */
 export default interface Concern
 {
+    /**
+     * Creates a new concern instance
+     * 
+     * @param {object} owner The target class instance this concern was injected into
+     */
+    constructor(owner: object);
     
+    /**
+     * Returns the target class instance this concern was injected into
+     * 
+     * @return {object}
+     */
+    get concernOwner(): object;
 }
