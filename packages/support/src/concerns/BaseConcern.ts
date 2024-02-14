eb90db4329c0cc3d911791d4105e6d2e601aa9a2
alin <alin@rspsystems.com>
2024-02-14 11:45:16 +0100

    Add abstract concern class

---
+++ packages/support/src/concerns/BaseConcern.ts
@@ -0,0 +1,60 @@
+import type { Concern } from "@aedart/contracts/support/concerns";
+import { HIDDEN, ALWAYS_HIDDEN } from "@aedart/contracts/support/concerns";
+
+/**
+ * Base Concern
+ * 
+ * Abstraction for a {@link Concern} component.
+ * 
+ * @implements {Concern}
+ * @abstract
+ */
+export default abstract class BaseConcern implements Concern
+{
+    /**
+     * The target class instance this concern is injected into
+     * 
+     * @private
+     * @type {object}
+     */
+    readonly #concernOwner: object;
+
+    /**
+     * Creates a new concern instance
+     *
+     * @param {object} owner The target class instance this concern is injected into
+     *
+     * @throws {Error} When concern is unable to preform initialisation, e.g. caused
+     *                 by the owner or other circumstances.
+     */
+    constructor(owner: object)
+    {
+        if (new.target === BaseConcern) {
+            throw new Error('Unable to instantiate new instance of abstract class BaseConcern');
+        }
+        
+        this.#concernOwner = owner;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    get concernOwner(): object
+    {
+        return this.#concernOwner;
+    }
+
+    /**
+     * Returns a list of properties and methods that MUST NOT be aliased into the target class.
+     * 
+     * **Warning**: _Regardless of what properties and methods this method may return,
+     * an "injector" component that applies this concern MUST ensure that the {@link ALWAYS_HIDDEN}
+     * defined properties and methods are **NEVER** aliased into a target class._
+     * 
+     * @return {PropertyKey[]}
+     */
+    static [HIDDEN](): PropertyKey[]
+    {
+        return ALWAYS_HIDDEN as PropertyKey[];
+    }
+}
