6f4a11fd25713ed4798bb4b5720abde1980b72db
alin <alin@rspsystems.com>
2024-02-14 11:32:08 +0100

    Add an "always hidden" array
    List defines what properties and methods should never be aliased into a target class.

--- packages/contracts/src/support/concerns/index.ts
+++ packages/contracts/src/support/concerns/index.ts
@@ -16,6 +16,25 @@ export const SUPPORT_CONCERNS: unique symbol = Symbol('@aedart/contracts/support
  */
 export const HIDDEN: unique symbol = Symbol('hidden');
 
+/**
+ * List of properties and methods that must always remain "hidden" and
+ * NEVER be aliased into a target class.
+ * 
+ * @type {ReadonlyArray<PropertyKey>}
+ */
+export const ALWAYS_HIDDEN: ReadonlyArray<PropertyKey> = [
+    // It is NOT possible, nor advised to attempt to alias a Concern's
+    // constructor into a target class.
+    'constructor',
+    
+    // The concernOwner property (getter) will not work either...
+    'concernOwner',
+    
+    // Lastly, if the Concern defines any hidden properties or methods,
+    // then such a method will not do any good in a target class.
+    HIDDEN
+];
+
 export {
     type Concern,
     type Configuration
