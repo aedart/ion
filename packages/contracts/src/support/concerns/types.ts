51b733c98c110747a18509a59db0a70ead321e0f
alin <alin@rspsystems.com>
2024-02-14 15:05:08 +0100

    Improve Aliases type definition

--- packages/contracts/src/support/concerns/types.ts
+++ packages/contracts/src/support/concerns/types.ts
@@ -1,12 +1,15 @@
 import Concern from "./Concern";
 
 /**
+ * An alias for a property or method in a {@link Concern} class
+ */
+export type Alias = PropertyKey;
+
+/**
  * A record that defines one or more aliases for a {@link Concern}'s properties or methods.
  * 
- * In this context an "alias" means a property or method name that is added onto a target
+ * In this context an "alias" means a property or method that is added onto a target
  * class' prototype and acts as a proxy to the original property or method inside the
- * concern class.
+ * concern class instance.
  */
-export type Aliases<T extends Concern> = {
-    [P in keyof T]: PropertyKey
-}
+export type Aliases<T extends Concern> = Record<T, Alias>;
