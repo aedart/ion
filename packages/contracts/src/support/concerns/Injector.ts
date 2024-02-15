b67e945b305ff83c7577ac786310dd40186c7ff4
alin <alin@rspsystems.com>
2024-02-15 10:11:59 +0100

    Redesign API for Injector

--- packages/contracts/src/support/concerns/Injector.ts
+++ packages/contracts/src/support/concerns/Injector.ts
@@ -1,6 +1,7 @@
 import { Constructor, ConstructorOrAbstractConstructor } from "@aedart/contracts";
 import Concern from "./Concern";
 import Configuration from "./Configuration";
+import Container from "./Container";
 
 /**
  * Concerns Injector
@@ -10,51 +11,73 @@ import Configuration from "./Configuration";
  * 
  * @see {Concern}
  */
-export default interface Injector<T extends ConstructorOrAbstractConstructor = object>
+export default interface Injector
 {
     /**
-     * Returns the target class that concerns must be injected into
+     * Inject one or more concerns into the target class and return the modified target.
      * 
-     * @return {ConstructorOrAbstractConstructor}
-     */
-    get target(): T;
-
-    /**
-     * Inject one or more concerns into the target class and return the modified target
+     * **Note**: _This method performs the following (in given order):_
      * 
-     * TODO: Details description what this method must and MUST NOT do...
-     * TODO: E.g. define a "CONCERNS" property / map / method in target class' prototype (with inherited concerns from parent classes?)
-     * TODO: E.g. must prevent duplicate concern injection target (full inheritance chain)
-     * TODO: E.g. must prevent name conflicts in aliases
+     * _**A**: Defines a new concerns {@link Container} in target class' prototype, via {@link defineContainer}._
      * 
+     * _**B**: Defines aliases (proxy properties and methods) in target class' prototype, via {@link defineAliases}._
+     * 
+     * @template T extends {@link ConstructorOrAbstractConstructor} = object
      * @template C = {@link Concern}
      * 
+     * @param {T} target The target class concerns must be injected into
      * @param {...Constructor<C>|Configuration<C>} concerns
      * 
-     * @return {ConstructorOrAbstractConstructor}
+     * @return {T} Given target class with concern classes injected into its prototype
      * 
-     * @throws {Error} If unable to inject concerns into target class
+     * @throws {TypeError|Error}
      */
-    inject<C = Concern>(...concerns: Constructor<C>|Configuration<C>): T;
-    
-    /**
-     * Create aliases (a proxies) in target class, to the properties or methods in given concern
-     * 
-     * **Note**: _Method creates each alias using the {@link createAlias} method!_
-     * 
-     * @param {Configuration<Concern>} config The concern injection configuration
-     *
-     * @throws {Error} If unable to obtain keys from source concern or failure occurs when defining
-     *                 proxy properties or methods in target class
-     */
-    createAliases(config: Configuration<Concern>): void;
+    inject<
+        T extends ConstructorOrAbstractConstructor = object,
+        C = Concern
+    >(target: T, ...concerns: Constructor<C>|Configuration<C>): T;
 
     /**
-     * Create an alias (a proxy) in target class, to a property or method in given concern
+     * Defines a concerns {@link Container} in target class' prototype using
+     * [CONCERNS]{@link import('@aedart/contracts/support/concerns').CONCERNS} as its property key.
      * 
-     * **Note**: _Method will do nothing if a property or method exists in the target, with the same name
-     * as the given alias!_
+     * **Note**: _If target class has a parent that already has a container defined, then the
+     * concern classes it contains will be merged with those provided as argument for this method,
+     * and populated into the new container._
      * 
+     * @param {object} target The target class in which a concerns container must be defined
+     * @param {Constructor<Concern>[]} concerns The concern classes to populate the container with.
+     *
+     * @return {Container}
+     * 
+     * @throws {TypeError} If duplicate concern classes are provided, or if provided concern classes are already
+     *                     defined in target class' parent.
+     * @throws {Error} If unable to define concerns container in target class.
+     */
+    defineContainer(target: object, concerns: Constructor<Concern>[]): Container;
+    
+    /**
+     * Create aliases (proxy properties and methods) in target class' prototype, to the properties
+     * or methods in given concerns.
+     * 
+     * **Note**: _Method creates each alias using the {@link defineAlias} method!_
+     * 
+     * @param {object} target The target class aliases must be created in
+     * @param {Configuration<Concern>[]} configurations List of concern injection configurations
+     *
+     * @throws {TypeError} In case of conflicting aliases.
+     * @throws {Error} If unable to obtain keys from source concern or failure occurs when defining
+     *                 proxy properties or methods in target class' prototype.
+     */
+    defineAliases(target: object, configurations: Configuration<Concern>[]): void;
+
+    /**
+     * Create an alias (a proxy) in target class' prototype, to a property or method in given concern.
+     * 
+     * **Note**: _Method will do nothing if a property or method already exists in the target, with the same
+     * name as the given alias!_
+     *
+     * @param {object} target The target class the alias must be created in
      * @param {PropertyKey} key Name of the property or method in the source concern class to create alias for
      * @param {PropertyKey} alias Alias for the key to create in the target class (the proxy property or method)
      * @param {Constructor<Concern>} source The concern to that the alias property or method must proxy to 
@@ -64,7 +87,12 @@ export default interface Injector<T extends ConstructorOrAbstractConstructor = o
      *                   already exists in target. 
      * 
      * @throws {Error} If unable to obtain key from source concern or failure occurs when defining
-     *                 proxy property or method in target
+     *                 proxy property or method in target class' prototype.
      */
-    createAlias(key: PropertyKey, alias: PropertyKey, source: Constructor<Concern>): boolean;
+    defineAlias(
+        target: object,
+        key: PropertyKey,
+        alias: PropertyKey,
+        source: Constructor<Concern>
+    ): boolean;
 }
