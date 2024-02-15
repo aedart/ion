016bbed61e007307b7c48066b78bac1bb818dc77
Alin Eugen Deac <aedart@gmail.com>
2024-02-15 18:33:36 +0100

    Add reference to Mozilla docs

--- packages/contracts/src/support/reflections/index.ts
+++ packages/contracts/src/support/reflections/index.ts
@@ -8,6 +8,8 @@ export const SUPPORT_REFLECTIONS: unique symbol = Symbol('@aedart/contracts/supp
 /**
  * The prototype of {@link Function}
  * 
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/prototype
+ * 
  * @type {object}
  */
 export const FUNCTION_PROTOTYPE: object = Reflect.getPrototypeOf(Function);
