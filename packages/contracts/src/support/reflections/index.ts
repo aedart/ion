8b785fcf70b5480bfaea4c8bdfbd7d30a466951f
Alin Eugen Deac <aedart@gmail.com>
2024-02-15 18:18:33 +0100

    Add prototype of Function as const in contracts

--- packages/contracts/src/support/reflections/index.ts
+++ packages/contracts/src/support/reflections/index.ts
@@ -4,3 +4,10 @@
  * @type {Symbol}
  */
 export const SUPPORT_REFLECTIONS: unique symbol = Symbol('@aedart/contracts/support/reflections');
+
+/**
+ * The prototype of {@link Function}
+ * 
+ * @type {object}
+ */
+export const FUNCTION_PROTOTYPE: object = Reflect.getPrototypeOf(Function);
