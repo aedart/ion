ed8add3f176ebfe152e5447986bdfdac1986154f
alin <alin@rspsystems.com>
2024-02-16 14:31:58 +0100

    Add boot and not registered exception interfaces

--- packages/contracts/src/support/concerns/exceptions/index.ts
+++ packages/contracts/src/support/concerns/exceptions/index.ts
@@ -1,4 +1,8 @@
+import BootException from "./BootException";
 import ConcernException from "./ConcernException";
+import NotRegisteredException from "./NotRegisteredException";
 export {
-    type ConcernException
+    type BootException,
+    type ConcernException,
+    type NotRegisteredException
 }
