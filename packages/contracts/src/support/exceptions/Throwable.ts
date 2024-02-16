cfbfc9a762f94b6cdb1763134181720df94ef6a9
alin <alin@rspsystems.com>
2024-02-16 11:44:33 +0100

    Add reference to PHP's Throwable

--- packages/contracts/src/support/exceptions/Throwable.ts
+++ packages/contracts/src/support/exceptions/Throwable.ts
@@ -3,8 +3,10 @@
  * 
  * Base interface for a custom {@link Error} that can be thrown.
  * 
+ * This interface is inspired by PHP's [`Throwable` interface]{@link https://www.php.net/manual/en/class.throwable.php}.
+ * 
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#custom_error_types
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+ * @see Error
  */
-export default interface Throwable extends Error
-{}
+export default interface Throwable extends Error {}
