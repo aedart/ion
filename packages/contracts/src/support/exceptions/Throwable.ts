8221e4ea9d87905db716a91601eea69f8cd07316
alin <alin@rspsystems.com>
2024-02-16 11:39:51 +0100

    Add Throwable interface

---
+++ packages/contracts/src/support/exceptions/Throwable.ts
@@ -0,0 +1,10 @@
+/**
+ * Throwable
+ * 
+ * Base interface for a custom {@link Error} that can be thrown.
+ * 
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error#custom_error_types
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+ */
+export default interface Throwable extends Error
+{}
--- packages/contracts/src/support/exceptions/index.ts
+++ packages/contracts/src/support/exceptions/index.ts
@@ -4,3 +4,8 @@
  * @type {Symbol}
  */
 export const SUPPORT_EXCEPTIONS: unique symbol = Symbol('@aedart/contracts/support/exceptions');
+
+import Throwable from "./Throwable";
+export {
+    type Throwable
+}
