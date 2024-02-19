6baf7bc14f9ef3fbd8a6a5d680824f347f7449da
alin <alin@rspsystems.com>
2024-02-19 16:25:23 +0100

    Add ConcatSpreaddable interface

--- packages/contracts/src/support/arrays/index.ts
+++ packages/contracts/src/support/arrays/index.ts
@@ -4,3 +4,8 @@
  * @type {Symbol}
  */
 export const SUPPORT_ARRAYS: unique symbol = Symbol('@aedart/contracts/support/arrays');
+
+import ConcatSpreadable from "./ConcatSpreadable";
+export {
+    type ConcatSpreadable
+}
