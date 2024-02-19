809db15cde5d0c90c83ab7e83e66f8a5981be1bc
alin <alin@rspsystems.com>
2024-02-19 09:26:26 +0100

    Add examples for options

--- packages/contracts/src/support/objects/MergeOptions.ts
+++ packages/contracts/src/support/objects/MergeOptions.ts
@@ -26,6 +26,18 @@ export default interface MergeOptions
      * **Callback**: _A callback can be specified to determine if a given key,
      * in a source object should be skipped._
      * 
+     * **Example:**
+     * ```js
+     * const a { 'foo': true };
+     * const a { 'bar': true, 'zar': true };
+     * 
+     * merge([ a, b ], { skip: [ 'zar' ] }); // { 'foo': true, 'bar': true }
+     * 
+     * merge([ a, b ], { skip: (key, source) => {
+     *      return key === 'bar' && Reflect.has(source, key);
+     * } }); // { 'foo': true, 'zar': true }
+     * ```
+     * 
      * @type {PropertyKey[] | SkipKeyCallback}
      */
     skip?: PropertyKey[] | SkipKeyCallback;
@@ -38,6 +50,16 @@ export default interface MergeOptions
      * 
      * **When `false`**: _If an existing property value is not `undefined`, it will NOT be overwritten
      * with new value, if the new value is `undefined`._
+     *
+     * **Example:**
+     * ```js
+     * const a { 'foo': true };
+     * const a { 'foo': undefined };
+     *
+     * merge([ a, b ]); // { 'foo': undefined }
+     *
+     * merge([ a, b ], { overwriteWithUndefined: false }); // { 'foo': true }
+     * ```
      * 
      * @type {boolean}
      */
@@ -49,6 +71,16 @@ export default interface MergeOptions
      * **When `true`**: _existing array property value is attempted merged with new array value._
      *
      * **When `false` (_default behaviour_)**: _existing array property value is overwritten with new array value_
+     *
+     * **Example:**
+     * ```js
+     * const a { 'foo': [ 1, 2, 3 ] };
+     * const a { 'foo': [ 4, 5, 6 ] };
+     *
+     * merge([ a, b ]); // { 'foo': [ 4, 5, 6 ] }
+     *
+     * merge([ a, b ], { mergeArrays: true }); // { 'foo': [ 1, 2, 3, 4, 5, 6 ] }
+     * ```
      * 
      * @type {boolean}
      */
@@ -57,8 +89,8 @@ export default interface MergeOptions
     /**
      * The merge callback that must be applied
      * 
-     * **Note**: _When no callback is provided, then a default is
-     * set by the merge function_
+     * **Note**: _When no callback is provided, then the merge function's default
+     * callback is used._
      * 
      * @type {MergeCallback}
      */
