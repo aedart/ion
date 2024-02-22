81f84dfac4b459cb7964518909004c86e875541f
Alin Eugen Deac <aedart@gmail.com>
2024-02-22 21:07:43 +0100

    Add default implementation for [PROVIDES]() static method
    Also, added a way to cache the keys. This should increase the performance a bit, whenever the same concern class is used across multiple components.

--- packages/support/src/concerns/AbstractConcern.ts
+++ packages/support/src/concerns/AbstractConcern.ts
@@ -1,16 +1,39 @@
-import type { Concern } from "@aedart/contracts/support/concerns";
-import { HIDDEN, ALWAYS_HIDDEN } from "@aedart/contracts/support/concerns";
+import type {
+    Concern,
+    ConcernConstructor
+} from "@aedart/contracts/support/concerns";
+import {
+    HIDDEN,
+    PROVIDES,
+    ALWAYS_HIDDEN
+} from "@aedart/contracts/support/concerns";
 import { AbstractClassError } from "@aedart/support/exceptions";
+import { classOwnKeys } from "@aedart/support/reflections";
+import {Constructor} from "@aedart/contracts";
 
 /**
  * Abstract Concern
  * 
  * @see {Concern}
+ * @see {ConcernConstructor}
+ * 
  * @implements {Concern}
+ * 
  * @abstract
  */
 export default abstract class AbstractConcern implements Concern
 {
+    /**
+     * In-memory cache of resolved keys (properties and methods), which
+     * are offered by concern(s) and can be aliased. 
+     * 
+     * @type {WeakMap<ThisType<ConcernConstructor>, PropertyKey[]>}
+     * 
+     * @protected
+     * @static
+     */
+    protected static resolvedConcernKeys: WeakMap<ThisType<ConcernConstructor>, PropertyKey[]> = new WeakMap();
+    
     /**
      * The owner class instance this concern is injected into,
      * or `this` concern instance.
@@ -67,5 +90,75 @@ export default abstract class AbstractConcern implements Concern
     static [HIDDEN](): ReadonlyArray<PropertyKey>
     {
         return ALWAYS_HIDDEN;
+    }
+
+    /**
+     * Returns list of property keys that this concern class offers.
+     *
+     * **Note**: _Only properties and methods returned by this method can be aliased
+     * into a target class._
+     *
+     * @static
+     *
+     * @return {PropertyKey[]}
+     */
+    public static [PROVIDES](): PropertyKey[]
+    {
+        // Feel free to overwrite this static method in your concern class and specify
+        // the properties and methods that your concern offers (those that can be aliased).
+        
+        return this.rememberConcernKeys(this, () => {
+            return this.removeAlwaysHiddenKeys(
+                classOwnKeys(this, true)
+            );
+        });
+    }
+
+    /**
+     * Removes keys that should remain hidden
+     * 
+     * @see ALWAYS_HIDDEN
+     * 
+     * @param {PropertyKey[]} keys
+     * 
+     * @returns {PropertyKey[]}
+     * 
+     * @protected
+     * @static
+     */
+    protected static removeAlwaysHiddenKeys(keys: PropertyKey[]): PropertyKey[]
+    {
+        return keys.filter((key: PropertyKey) => {
+            return !ALWAYS_HIDDEN.includes(key);
+        });
+    }
+
+    /**
+     * Remember the resolved keys (properties and methods) for given target concern class
+     * 
+     * @param {ThisType<ConcernConstructor>} concern
+     * @param {() => PropertyKey[]} callback
+     * @param {boolean} [force=false] 
+     * 
+     * @returns {PropertyKey[]}
+     * 
+     * @protected
+     * @static
+     */
+    protected static rememberConcernKeys(
+        concern: ThisType<ConcernConstructor>,
+        callback: () => PropertyKey[],
+        force: boolean = false
+    ): PropertyKey[]
+    {
+        if (!force && this.resolvedConcernKeys.has(concern)) {
+            return this.resolvedConcernKeys.get(concern) as PropertyKey[]; 
+        }
+        
+        const keys: PropertyKey[] = callback();
+        
+        this.resolvedConcernKeys.set(concern, keys);
+        
+        return keys;
     }
 }
