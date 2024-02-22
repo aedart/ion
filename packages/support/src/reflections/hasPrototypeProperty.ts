4a67abc072dcda7531a798868e0b5b3a108193a9
alin <alin@rspsystems.com>
2024-02-22 13:19:10 +0100

    Fix returns true for null argument

--- packages/support/src/reflections/hasPrototypeProperty.ts
+++ packages/support/src/reflections/hasPrototypeProperty.ts
@@ -1,3 +1,6 @@
+import {isset} from "@aedart/support/misc";
+
+
 /**
  * Determine if target object has a prototype property defined
  * 
@@ -5,13 +8,15 @@
  * _The method literally checks if a "prototype" property is defined in target, that it is not `null` or `undefined`,
  * and that its of the [type]{@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof} 'object'!_
  * 
+ * **Note**: _Method returns `false` if `null` given as argument!_
+ * 
  * @param {object} target
  * 
  * @returns {boolean}
  */
 export function hasPrototypeProperty(target: object): boolean
 {
-    return target
+    return isset(target)
         && typeof (target as Record<PropertyKey, any>)['prototype'] == 'object'
         && (target as Record<PropertyKey, any>)['prototype'] !== null;
 }
