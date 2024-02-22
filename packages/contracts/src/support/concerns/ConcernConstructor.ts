279e3a6e5eff263e1bd3653280076220bd146521
alin <alin@rspsystems.com>
2024-02-22 16:09:22 +0100

    Add Concern Constructor interface

---
+++ packages/contracts/src/support/concerns/ConcernConstructor.ts
@@ -0,0 +1,35 @@
+import Concern from "./Concern";
+import { PROPERTIES } from "./index";
+
+/**
+ * Concern Constructor
+ * 
+ * @template T extends Concern
+ * 
+ * @see Concern
+ */
+export default interface ConcernConstructor<T extends Concern>
+{
+    /**
+     * Creates a new concern instance
+     *
+     * @param {object} [owner] The owner class instance this concern is injected into.
+     *                         Defaults to `this` concern instance if none given.
+     *
+     * @throws {Error} When concern is unable to preform initialisation, e.g. caused
+     *                 by the owner or other circumstances.
+     */
+    new (owner?: object): T;
+
+    /**
+     * Returns list of property keys that this concern class offers
+     * 
+     * **Note**: _Only properties and methods returned by this method can be aliased
+     * into a target class._
+     * 
+     * @static
+     * 
+     * @return {PropertyKey[]}
+     */
+    [PROPERTIES](): (keyof T)[];
+}
