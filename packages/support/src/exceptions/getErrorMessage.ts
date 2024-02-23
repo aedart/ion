d600d94ef1ea35bb4260b512466779fa821a1efb
alin <alin@rspsystems.com>
2024-02-23 09:54:28 +0100

    Replace unknown with any type

--- packages/support/src/exceptions/getErrorMessage.ts
+++ packages/support/src/exceptions/getErrorMessage.ts
@@ -1,12 +1,15 @@
 /**
  * Returns error message from {@link Error}, if possible
  *
- * @param {unknown} error Error or value that was thrown
+ * @param {any} error Error or value that was thrown
  * @param {string} [defaultMessage] A default message to return if unable to resolve error message
  *
  * @return {string}
  */
-export function getErrorMessage(error: unknown, defaultMessage: string = 'unknown reason'): string
+export function getErrorMessage(
+    error: any, /* eslint-disable-line @typescript-eslint/no-explicit-any */
+    defaultMessage: string = 'unknown reason'
+): string
 {
     return (typeof error == 'object' && error instanceof Error && Reflect.has(error, 'message'))
         ? error.message
