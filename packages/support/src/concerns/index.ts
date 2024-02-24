2b0a9b7a9f54514500580e6c3f06054e9092869c
Alin Eugen Deac <aedart@gmail.com>
2024-02-24 19:15:59 +0100

    Add UNSAFE_PROPERTY_KEYS
    This is the implementation specific replacement for the HIDDEN const in concern contracts.

--- packages/support/src/concerns/index.ts
+++ packages/support/src/concerns/index.ts
@@ -1,7 +1,47 @@
+import {DANGEROUS_PROPERTIES} from "@aedart/contracts/support/objects";
+import {
+    CONCERN_CLASSES,
+    CONCERNS,
+    PROVIDES
+} from "@aedart/contracts/support/concerns";
+
+/**
+ * List of "unsafe" property keys, which should NOT be aliased
+ * 
+ * @type {PropertyKey[]}
+ */
+export const UNSAFE_PROPERTY_KEYS: PropertyKey[] = [
+    ...DANGEROUS_PROPERTIES,
+
+    // ----------------------------------------------------------------- //
+    // Defined by Concern interface / Abstract Concern:
+    // ----------------------------------------------------------------- //
+
+    // It is NOT possible, nor advised to attempt to alias a Concern's
+    // constructor into a target class.
+    'constructor',
+
+    // The concernOwner property (getter) shouldn't be aliased either
+    'concernOwner',
+    
+    // The static properties and methods (just in case...)
+    PROVIDES,
+
+    // ----------------------------------------------------------------- //
+    // Other properties and methods:
+    // ----------------------------------------------------------------- //
+
+    // In case that a concern class uses other concerns, prevent them
+    // from being aliased.
+    CONCERN_CLASSES,
+    CONCERNS,
+]
+
 import AbstractConcern from "./AbstractConcern";
 import { ConcernClassBlueprint } from "./ConcernClassBlueprint";
 import ConcernsContainer from "./ConcernsContainer";
 import ConcernsInjector from "./ConcernsInjector";
+
 
 export {
     AbstractConcern,
