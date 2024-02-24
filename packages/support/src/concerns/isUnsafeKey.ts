cf15261d480801985b4652885f3ef77f3b893d54
Alin Eugen Deac <aedart@gmail.com>
2024-02-24 21:17:16 +0100

    (Re)add UNSAFE_PROPERTY_KEYS const
    Normally, this could be inside the index.ts file, but this works too.

--- packages/support/src/concerns/isUnsafeKey.ts
+++ packages/support/src/concerns/isUnsafeKey.ts
@@ -2,6 +2,36 @@ import { DANGEROUS_PROPERTIES } from "@aedart/contracts/support/objects";
 import { CONCERN_CLASSES, CONCERNS, PROVIDES } from "@aedart/contracts/support/concerns";
 
 /**
+ * List of property keys that are considered "unsafe" to alias (proxy to)
+ */
+export const UNSAFE_PROPERTY_KEYS = [
+    ...DANGEROUS_PROPERTIES,
+
+    // ----------------------------------------------------------------- //
+    // Defined by Concern interface / Abstract Concern:
+    // ----------------------------------------------------------------- //
+
+    // It is NOT possible, nor advised to attempt to alias a Concern's
+    // constructor into a target class.
+    'constructor',
+
+    // The concernOwner property (getter) shouldn't be aliased either
+    'concernOwner',
+
+    // The static properties and methods (just in case...)
+    PROVIDES,
+
+    // ----------------------------------------------------------------- //
+    // Other properties and methods:
+    // ----------------------------------------------------------------- //
+
+    // In case that a concern class uses other concerns, prevent them
+    // from being aliased.
+    CONCERN_CLASSES,
+    CONCERNS,
+];
+
+/**
  * Determine if given property key is considered "unsafe"
  * 
  * @see UNSAFE_PROPERTY_KEYS
@@ -12,30 +42,5 @@ import { CONCERN_CLASSES, CONCERNS, PROVIDES } from "@aedart/contracts/support/c
  */
 export function isUnsafeKey(key: PropertyKey): boolean
 {
-    return [
-        ...DANGEROUS_PROPERTIES,
-    
-        // ----------------------------------------------------------------- //
-        // Defined by Concern interface / Abstract Concern:
-        // ----------------------------------------------------------------- //
-    
-        // It is NOT possible, nor advised to attempt to alias a Concern's
-        // constructor into a target class.
-        'constructor',
-    
-        // The concernOwner property (getter) shouldn't be aliased either
-        'concernOwner',
-    
-        // The static properties and methods (just in case...)
-        PROVIDES,
-    
-        // ----------------------------------------------------------------- //
-        // Other properties and methods:
-        // ----------------------------------------------------------------- //
-    
-        // In case that a concern class uses other concerns, prevent them
-        // from being aliased.
-        CONCERN_CLASSES,
-        CONCERNS,
-    ].includes(key);
+    return UNSAFE_PROPERTY_KEYS.includes(key);
 }
