15b2daae4f9085ce08d609a86078d28668da086e
Alin Eugen Deac <aedart@gmail.com>
2024-02-25 14:15:52 +0100

    Improve description of defineAlias()
    Also removed the "C extends Concern" generic - no longer needed.

--- packages/contracts/src/support/concerns/Injector.ts
+++ packages/contracts/src/support/concerns/Injector.ts
@@ -1,4 +1,3 @@
-import Concern from "./Concern";
 import ConcernConstructor from "./ConcernConstructor";
 import Configuration from "./Configuration";
 import UsesConcerns from "./UsesConcerns";
@@ -86,40 +85,39 @@ export default interface Injector<T = object>
      * @template T = object
      * 
      * @param {UsesConcerns<T>} target The target in which "aliases" must be defined in
-     * @param {Configuration<Concern>[]} configurations List of concern injection configurations
+     * @param {Configuration[]} configurations List of concern injection configurations
      * 
      * @returns {UsesConcerns<T>} The modified target class
      *
      * @throws {AliasConflictException} If case of alias naming conflicts.
      * @throws {InjectionException} If unable to define aliases in target class.
      */
-    defineAliases<T = object>(target: UsesConcerns<T>, configurations: Configuration<Concern>[]): UsesConcerns<T>;
+    defineAliases<T = object>(target: UsesConcerns<T>, configurations: Configuration[]): UsesConcerns<T>;
 
     /**
-     * Defines an "alias" (proxy property or method) in target class' prototype, to a property or method
-     * in given concern.
+     * Defines an "alias" (proxy property or method) in target class' prototype, which points to a property or method
+     * in the given concern.
      * 
      * **Note**: _Method will do nothing, if a property or method already exists in the target class' prototype
      * chain, with the same name as given "alias"._
-     * 
-     * @template C extends Concern
+     *
      * @template T = object
      * 
      * @param {UsesConcerns<T>} target The target in which "alias" must be defined in
      * @param {PropertyKey} alias Name of the "alias" in the target class (name of the proxy property or method)
-     * @param {PropertyKey} key Name of the property or method that the "alias" is for, in the concern class (`source`)
-     * @param {Constructor<Concern>} source The concern class that holds the property or methods (`key`)
+     * @param {PropertyKey} key Name of the property or method that the "alias" points to, in the concern class (`source`)
+     * @param {Constructor} source The source concern class that contains the property or methods that is pointed to (`key`)
      * 
-     * @returns {boolean} `true` if "alias" was in target class. `false` if not, e.g. a property or method already
-     *                    exists in target class' prototype chain, with the same name as the alias.
+     * @returns {boolean} `true` if "alias" was in target class. `false` if a property or method already exists in the
+     *                     target, with the same name as the "alias".
      *
-     * @throws {UnsafeAliasException} If an alias points to an "unsafe" property or method in concern
+     * @throws {UnsafeAliasException} If an alias points to an "unsafe" property or method in the source concern class.
      * @throws {InjectionException} If unable to define "alias" in target class.
      */
-    defineAlias<C extends Concern, T = object>(
+    defineAlias<T = object>(
         target: UsesConcerns<T>,
         alias: PropertyKey,
         key: PropertyKey,
-        source: ConcernConstructor<C>
+        source: ConcernConstructor
     ): boolean;
 }
