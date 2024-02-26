9e863242d9c76920f5dd544e50f93eb3ed934bc0
alin <alin@rspsystems.com>
2024-02-26 15:02:25 +0100

    Refactor, use new Proxy Descriptor Resolver

--- packages/support/src/concerns/ConcernsInjector.ts
+++ packages/support/src/concerns/ConcernsInjector.ts
@@ -9,6 +9,7 @@ import {
     Factory,
     Alias,
     Aliases,
+    Resolver,
 } from "@aedart/contracts/support/concerns";
 import type { ConstructorOrAbstractConstructor } from "@aedart/contracts";
 import {
@@ -28,6 +29,7 @@ import ConcernsContainer from './ConcernsContainer';
 import ConfigurationFactory from "./ConfigurationFactory";
 import Descriptors from "./Descriptors";
 import { isUnsafeKey } from "./isUnsafeKey";
+import ProxyResolver from "./ProxyResolver";
 
 /**
  * A map of the concern owner instances and their concerns container
@@ -74,6 +76,15 @@ export default class ConcernsInjector<T = object> implements Injector<T>
      * @protected
      */
     protected descriptors: DescriptorsCache;
+
+    /**
+     * Proxy Descriptor Resolver
+     * 
+     * @type {Resolver}
+     * 
+     * @protected
+     */
+    protected proxyResolver: Resolver;
     
     /**
      * Create a new Concerns Injector instance
@@ -87,6 +98,7 @@ export default class ConcernsInjector<T = object> implements Injector<T>
         this.#target = target;
         this.factory = this.makeConfigurationFactory();
         this.descriptors = this.makeDescriptorsCache();
+        this.proxyResolver = this.makeProxyResolver();
     }
     
     /**
@@ -297,7 +309,7 @@ export default class ConcernsInjector<T = object> implements Injector<T>
         }
 
         // Define the proxy property or method, using the concern's property descriptor to determine what must be defined.
-        const proxy = this.resolveProxyDescriptor(key, source, concernDescriptors[key])
+        const proxy = this.proxyResolver.resolve(key, source, concernDescriptors[key]);
 
         return this.definePropertyInTarget<T>(target.prototype, alias, proxy) !== undefined;
     }
@@ -554,115 +566,6 @@ export default class ConcernsInjector<T = object> implements Injector<T>
             throw new AliasConflictError(target, concern, alias, key, source);
         }
     }
-    
-    /**
-     * Resolves the proxy property descriptor for given key in source concern
-     *
-     * @param {PropertyKey} key
-     * @param {ConcernConstructor} source
-     * @param {PropertyDescriptor} keyDescriptor Descriptor of `key` in `source`
-     *
-     * @returns {PropertyDescriptor} Descriptor to be used for defining alias in a target class
-     *
-     * @protected
-     */
-    protected resolveProxyDescriptor(key: PropertyKey, source: ConcernConstructor, keyDescriptor: PropertyDescriptor): PropertyDescriptor
-    {
-        const proxy: PropertyDescriptor = Object.assign(Object.create(null), {
-            configurable: keyDescriptor.configurable,
-            enumerable: keyDescriptor.enumerable,
-            // writable: keyDescriptor.writable // Do not specify here...            
-        });
-
-        // A descriptor can only have an accessor, a value or writable attribute. Depending on the "value"
-        // a different kind of proxy must be defined.
-        const hasValue: boolean = Reflect.has(keyDescriptor, 'value');
-
-        if (hasValue && typeof keyDescriptor.value == 'function') {
-            proxy.value = this.makeMethodProxy(key, source);
-        } else if (hasValue) {
-            // When value is not a function, it could be a readonly property...
-            proxy.get = this.makeGetPropertyProxy(key, source);
-
-            // However, if the descriptor claims that its writable, then
-            // a setter must be defined too.
-            if (keyDescriptor.writable) {
-                proxy.set = this.makeSetPropertyProxy(key, source);
-            }
-        } else {
-            // Otherwise, the property can a getter and or a setter...
-            if (Reflect.has(keyDescriptor, 'get')) {
-                proxy.get = this.makeGetPropertyProxy(key, source);
-            }
-
-            if (Reflect.has(keyDescriptor, 'set')) {
-                proxy.set = this.makeSetPropertyProxy(key, source);
-            }
-        }
-
-        return proxy;
-    }
-    
-    /**
-     * Returns a new proxy "method" for given method in this concern
-     *
-     * @param {PropertyKey} method
-     * @param {ConcernConstructor} concern
-     *
-     * @returns {(...args: any[]) => any}
-     *
-     * @protected
-     */
-    protected makeMethodProxy(method: PropertyKey, concern: ConcernConstructor)
-    {
-        return function(
-            ...args: any[] /* eslint-disable-line @typescript-eslint/no-explicit-any */
-        ): any /* eslint-disable-line @typescript-eslint/no-explicit-any */
-        {
-            // @ts-expect-error This = concern instance
-            return (this as Owner)[CONCERNS].call(concern, method, ...args);
-        }
-    }
-
-    /**
-     * Returns a new proxy "get" for given property in this concern
-     *
-     * @param {PropertyKey} property
-     * @param {ConcernConstructor} concern
-     *
-     * @returns {() => any}
-     *
-     * @protected
-     */
-    protected makeGetPropertyProxy(property: PropertyKey, concern: ConcernConstructor)
-    {
-        return function(): any /* eslint-disable-line @typescript-eslint/no-explicit-any */
-        {
-            // @ts-expect-error This = concern instance
-            return (this as Owner)[CONCERNS].getProperty(concern, property);
-        }
-    }
-
-    /**
-     * Returns a new proxy "set" for given property in this concern
-     *
-     * @param {PropertyKey} property
-     * @param {ConcernConstructor} concern
-     *
-     * @returns {(value: any) => void}
-     *
-     * @protected
-     */
-    protected makeSetPropertyProxy(property: PropertyKey, concern: ConcernConstructor)
-    {
-        return function(
-            value: any /* eslint-disable-line @typescript-eslint/no-explicit-any */
-        ): void
-        {
-            // @ts-expect-error This = concern instance
-            (this as Owner)[CONCERNS].setProperty(concern, property, value);
-        }
-    }
 
     /**
      * Returns a new Descriptors (cache) instance
@@ -676,6 +579,18 @@ export default class ConcernsInjector<T = object> implements Injector<T>
         return new Descriptors();
     }
 
+    /**
+     * Returns a new Proxy Descriptor Resolver
+     * 
+     * @return {Resolver}
+     * 
+     * @protected
+     */
+    protected makeProxyResolver(): Resolver
+    {
+        return new ProxyResolver();
+    }
+    
     /**
      * Returns a new concern configuration factory instance
      * 
