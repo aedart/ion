ba0452de53b436eed0568b93e465ad4987d5edfa
alin <alin@rspsystems.com>
2024-02-29 10:48:53 +0100

    Change isSafeArrayLike to exclude plain strings values
    Also added tests for isSafeArrayLike

--- packages/support/src/arrays/isSafeArrayLike.ts
+++ packages/support/src/arrays/isSafeArrayLike.ts
@@ -5,15 +5,21 @@ import { isTypedArray } from './isTypedArray';
  * Determine if value is "safe" array-like object
  *
  * **Note**: _In this context "safe" means that given object passes {@link isArrayLike},
- * but is not instance of a {@link String} object, nor is the object a [Typed Array]{@link isTypedArray}!_
+ * but value is:_
+ *  - not a string.
+ *  - not instance of a {@link String} object.
+ *  - not a [Typed Array]{@link isTypedArray} object.
  * 
  * @param {object} value
  *
  * @return {boolean}
  */
-export function isSafeArrayLike(value: object): boolean
+export function isSafeArrayLike(
+    value: any /* eslint-disable-line @typescript-eslint/no-explicit-any */
+): boolean
 {
-    return isArrayLike(value)
+    return typeof value != 'string'
         && !(value instanceof String)
-        && !isTypedArray(value);
+        && !isTypedArray(value)
+        && isArrayLike(value);
 }
--- tests/browser/packages/support/arrays/isArrayLike.test.js
+++ tests/browser/packages/support/arrays/isArrayLike.test.js
@@ -1,4 +1,4 @@
-import { isArrayLike } from "@aedart/support/arrays";
+import { isArrayLike, isSafeArrayLike } from "@aedart/support/arrays";
 
 describe('@aedart/support/arrays', () => {
     describe('isArrayLike()', () => {
@@ -33,6 +33,39 @@ describe('@aedart/support/arrays', () => {
 
             for (const data of dataSet) {
                 expect(isArrayLike(data.value))
+                    .withContext(`${data.name} was expected to ${data.expected.toString()}`)
+                    .toBe(data.expected);
+            }
+        });
+
+        it('can determine if is "safe" array-like', () => {
+
+            const dataSet = [
+                { value: [], expected: true, name: 'Array' },
+                { value: { length: 0 }, expected: true, name: 'Object (with length property)' },
+
+                // -------------------------------------------------------------------------------- //
+                { value: 'abc', expected: false, name: 'string' },
+                { value: new String('abc'), expected: false, name: 'String (object)' },
+                { value: new Int8Array(), expected: false, name: 'TypedArray' },
+                
+                // -------------------------------------------------------------------------------- //
+                // These should never be considered array-like...
+
+                { value: new Boolean(true), expected: false, name: 'Boolean' },
+                { value: new Number(123), expected: false, name: 'Number' },
+                { value: {}, expected: false, name: 'Object (without length property)' },
+                { value: new Map(), expected: false, name: 'Map' },
+                { value: new Set(), expected: false, name: 'Set' },
+                { value: function() {}, expected: false, name: 'Function' },
+                { value: new Date(), expected: false, name: 'Date' },
+                { value: new ArrayBuffer(2), expected: false, name: 'ArrayBuffer' },
+                { value: new DataView(new ArrayBuffer(2)), expected: false, name: 'DataView' },
+                { value: new RegExp(/ab/g), expected: false, name: 'RegExp' },
+            ];
+
+            for (const data of dataSet) {
+                expect(isSafeArrayLike(data.value))
                     .withContext(`${data.name} was expected to ${data.expected.toString()}`)
                     .toBe(data.expected);
             }
