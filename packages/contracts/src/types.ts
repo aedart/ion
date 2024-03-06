78bc536a9515583e7e90b5ba04b4be6aadc1963c
alin <alin@rspsystems.com>
2024-03-06 13:52:34 +0100

    Add decorator result types
    This should make it easier to describe the return value(s) for decorator related utils.

--- packages/contracts/src/types.ts
+++ packages/contracts/src/types.ts
@@ -21,46 +21,86 @@ export type AbstractConstructor<T = object> = abstract new (...args: any[]) => T
 export type ConstructorOrAbstractConstructor<T = object> = Constructor<T> | AbstractConstructor<T>;
 
 /**
+ * Class Decorator Result
+ */
+export type ClassDecoratorResult = object | void;
+
+/**
+ * Class Method Decorator Result
+ */
+export type ClassMethodDecoratorResult = object | void;
+
+/**
+ * Class Getter Decorator Result
+ */
+export type ClassGetterDecoratorResult = object | void;
+
+/**
+ * Class Setter Decorator Result
+ */
+export type ClassSetterDecoratorResult = object | void;
+
+/**
+ * Class Field Decorator Result
+ */
+export type ClassFieldDecoratorResult = (initialValue: unknown) => unknown | void;
+
+/**
+ * Class Auto-Accessor Decorator Result
+ */
+export type ClassAutoAccessorDecoratorResult = ClassAccessorDecoratorResult<object, unknown> | void
+
+/**
+ * Decorator Result
+ */
+export type DecoratorResult = ClassDecoratorResult
+    | ClassMethodDecoratorResult
+    | ClassGetterDecoratorResult
+    | ClassSetterDecoratorResult
+    | ClassFieldDecoratorResult
+    | ClassAutoAccessorDecoratorResult;
+
+/**
  * Class Decorator
  *
  * @see https://github.com/tc39/proposal-decorators
  */
-export type ClassDecorator = (target: object, context: ClassDecoratorContext) => object | void;
+export type ClassDecorator = (target: object, context: ClassDecoratorContext) => ClassDecoratorResult;
 
 /**
  * Class Method Decorator
  *
  * @see https://github.com/tc39/proposal-decorators 
  */
-export type ClassMethodDecorator = (target: object, context: ClassMethodDecoratorContext) => object | void;
+export type ClassMethodDecorator = (target: object, context: ClassMethodDecoratorContext) => ClassMethodDecoratorResult;
 
 /**
  * Class Getter Decorator
  *
  * @see https://github.com/tc39/proposal-decorators
  */
-export type ClassGetterDecorator = (target: object, context: ClassGetterDecoratorContext) => object | void;
+export type ClassGetterDecorator = (target: object, context: ClassGetterDecoratorContext) => ClassGetterDecoratorResult;
 
 /**
  * Class Setter Decorator
  *
  * @see https://github.com/tc39/proposal-decorators
  */
-export type ClassSetterDecorator = (target: object, context: ClassSetterDecoratorContext) => object | void;
+export type ClassSetterDecorator = (target: object, context: ClassSetterDecoratorContext) => ClassSetterDecoratorResult;
 
 /**
  * Class Field Decorator
  *
  * @see https://github.com/tc39/proposal-decorators
  */
-export type ClassFieldDecorator = (target: object, context: ClassFieldDecoratorContext) => (initialValue: unknown) => unknown | void;
+export type ClassFieldDecorator = (target: object, context: ClassFieldDecoratorContext) => ClassFieldDecoratorResult;
 
 /**
  * Class Auto-Accessor Decorator
  *
  * @see https://github.com/tc39/proposal-decorators
  */
-export type ClassAutoAccessorDecorator = (target: ClassAccessorDecoratorTarget<object, unknown>, context: ClassAccessorDecoratorContext) => ClassAccessorDecoratorResult<object, unknown> | void
+export type ClassAutoAccessorDecorator = (target: ClassAccessorDecoratorTarget<object, unknown>, context: ClassAccessorDecoratorContext) => ClassAutoAccessorDecoratorResult;
 
 /**
  * Decorator
