87efda1dc302d09f336e840e159b524b062c7e57
alin <alin@rspsystems.com>
2024-03-07 11:40:52 +0100

    Change Entry, add static make method and resolve with prefix

--- packages/support/src/meta/Entry.ts
+++ packages/support/src/meta/Entry.ts
@@ -1,5 +1,6 @@
 import type { Key } from "@aedart/contracts/support";
 import type { MetaCallback, MetaEntry, MetaTargetContext } from "@aedart/contracts/support/meta";
+import { mergeKeys } from "@aedart/support/misc";
 
 /**
  * Meta Entry
@@ -34,6 +35,21 @@ export default class Entry implements MetaEntry
     }
 
     /**
+     * Create a new Meta Entry instance
+     * 
+     * @param {Key} key
+     * @param {unknown} value
+     * 
+     * @return {this|MetaEntry}
+     * 
+     * @static
+     */
+    public static make(key: Key, value: unknown): MetaEntry
+    {
+        return new this(key as Key, value);
+    }
+    
+    /**
      * Resolves given key and returns a new Meta Entry instance
      * 
      * @param {MetaTargetContext} targetContext
@@ -54,6 +70,32 @@ export default class Entry implements MetaEntry
             return (key as MetaCallback)(targetContext.target, targetContext.context, targetContext.owner);
         }
 
-        return new this(key as Key, value);
+        return this.make(key as Key, value);
+    }
+
+    /**
+     * Resolves given key-value pair and returns a new Meta Entry instance, with prefixed key 
+     * 
+     * @param {MetaTargetContext} targetContext
+     * @param {Key} prefixKey
+     * @param {Key|MetaCallback} key
+     * @param {unknown} [value]
+     * 
+     * @return {this|MetaEntry}
+     * 
+     * @static
+     */
+    public static resolveWithPrefix(
+        targetContext: MetaTargetContext,
+        prefixKey: Key,
+        key: Key | MetaCallback,
+        value?: any, /* eslint-disable-line @typescript-eslint/no-explicit-any */
+    ): MetaEntry
+    {
+        const entry = this.resolve(targetContext, key, value);
+
+        entry.key = mergeKeys(prefixKey, entry.key);
+        
+        return entry;
     }
 }
