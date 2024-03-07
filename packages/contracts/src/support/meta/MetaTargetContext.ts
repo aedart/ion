1b7cbb609f96c69ee1b69621b0986a69cb116903
alin <alin@rspsystems.com>
2024-03-07 09:34:38 +0100

    Improve property descriptions

--- packages/contracts/src/support/meta/MetaTargetContext.ts
+++ packages/contracts/src/support/meta/MetaTargetContext.ts
@@ -7,21 +7,29 @@ export default interface MetaTargetContext
 {
     /**
      * The class that owns the meta
+     * 
+     * @type {object}
      */
-    owner: object,
+    owner: object;
 
     /**
      * "This" argument
+     *
+     * @type {any}
      */
-    thisArg: any, /* eslint-disable-line @typescript-eslint/no-explicit-any */
+    thisArg: any; /* eslint-disable-line @typescript-eslint/no-explicit-any */
 
     /**
      * The target class, field, method... that is being decorated
+     *
+     * @type {object}
      */
-    target: object,
+    target: object;
 
     /**
      * Decorator context
+     *
+     * @type {Context}
      */
-    context: Context,
+    context: Context;
 }
