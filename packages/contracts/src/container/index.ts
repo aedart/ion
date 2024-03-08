330157b1837d11f55a057a297d65be6a76d7b9bf
alin <alin@rspsystems.com>
2024-03-08 09:11:01 +0100

    Add DEPENDENCIES symbol

--- packages/contracts/src/container/index.ts
+++ packages/contracts/src/container/index.ts
@@ -5,4 +5,14 @@
  */
 export const CONTAINER: unique symbol = Symbol('@aedart/contracts/container');
 
+/**
+ * Dependencies identifier
+ * 
+ * Symbol is intended to be used as an identifier for when associating binding identifiers
+ * or "concrete" dependencies with an element, e.g. a class, class method, function,...etc.
+ * 
+ * @type {symbol}
+ */
+export const DEPENDENCIES: unique symbol = Symbol('dependencies');
+
 export type * from './types';
