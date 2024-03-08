e7c15086c217f2eabd7eaf08be6ca1976855bf08
Alin Eugen Deac <aedart@gmail.com>
2024-03-08 20:41:37 +0100

    Add binding alias

--- packages/contracts/src/container/types.ts
+++ packages/contracts/src/container/types.ts
@@ -5,3 +5,10 @@
  * a service container.
  */
 export type Identifier = string | symbol | number | NonNullable<object>;
+
+/**
+ * Binding Alias
+ * 
+ * @see Identifier
+ */
+export type Alias = Identifier;
