c769df8b1fd770622fd4143e1ec366f11446c0dd
alin <alin@rspsystems.com>
2024-03-11 10:38:39 +0100

    Add ClassMethodName and ClassMethodReference type aliases

--- packages/contracts/src/types.ts
+++ packages/contracts/src/types.ts
@@ -21,3 +21,20 @@ export type AbstractConstructor<T = object> = abstract new (...args: any[]) => T
  * Constructor or Abstract Constructor type
  */
 export type ConstructorOrAbstractConstructor<T = object> = Constructor<T> | AbstractConstructor<T>;
+
+/**
+ * Class method name
+ */
+export type ClassMethodName<T = object> = {
+    [Name in keyof T]: T[Name] extends Function /* eslint-disable-line @typescript-eslint/ban-types */
+        ? Name
+        : never;
+}[keyof T];
+
+/**
+ * Class Method Reference
+ * 
+ * Array that contains either a class constructor or class instance, and the method name
+ * that must be processed at some point. E.g. the method to be invoked.
+ */
+export type ClassMethodReference<T = object> = [ Constructor<T> | T, ClassMethodName<T> ];
