4eb5749770ebd806806ba2b58bc621b4a0e9550b
alin <alin@rspsystems.com>
2024-03-11 16:06:07 +0100

    Add additional cases for built-in classes / objects

--- tests/browser/packages/support/reflections/isConstructor.test.js
+++ tests/browser/packages/support/reflections/isConstructor.test.js
@@ -21,24 +21,35 @@ describe('@aedart/support/reflections', () => {
             }
             
             const data = [
-                { value: null, expected: false },
-                { value: undefined, expected: false },
-                { value: /./, expected: false },
-                { value: {}, expected: false },
-                { value: [], expected: false },
-                { value: function() {}, expected: true },
-                { value: () => {}, expected: false },
-                { value: Array, expected: true },
-                { value: class {}, expected: true },
-                { value: classWithConstructor, expected: true },
-                { value: classWithStaticMethod.foo, expected: false },
+                { value: undefined, expected: false, name: 'undefined' },
+                { value: null, expected: false, name: 'null' },
+                { value: /./, expected: false, name: 'RegExp (as string)' },
+                { value: {}, expected: false, name: 'object' },
+                { value: [], expected: false, name: 'array' },
+                { value: () => {}, expected: false, name: 'function (arrow)' },
+
+                { value: function() {}, expected: true, name: 'function' },
+                { value: Array, expected: true, name: 'Array (object)' },
+                { value: String, expected: true, name: 'String (object)' },
+                { value: Number, expected: true, name: 'Number (object)' },
+                { value: Date, expected: true, name: 'Date (object)' },
+                { value: RegExp, expected: true, name: 'RegExp (object)' },
+                { value: Map, expected: true, name: 'Map (object)' },
+                { value: Set, expected: true, name: 'Set (object)' },
+                { value: WeakMap, expected: true, name: 'WeakMap (object)' },
+                { value: WeakSet, expected: true, name: 'WeakSet (object)' },
+                { value: WeakRef, expected: true, name: 'WeakRef (object)' },
+
+                { value: classWithConstructor, expected: true, name: 'class' },
+                { value: class {}, expected: true, name: 'class (anonymous)' },
+                { value: classWithStaticMethod.foo, expected: false, name: 'static class method' },
             ];
 
             data.forEach((entry, index) => {
 
                 let result = isConstructor(entry.value);
                 expect(result)
-                    .withContext(`Value at index ${index} was expected to be ${entry.expected}`)
+                    .withContext(`${entry.name} was expected to be ${entry.expected}`)
                     .toBe(entry.expected);
             });
         });
