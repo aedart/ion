72d2c3625fbcf5c90e6b5bce6cc382b403cf57fc
alin <alin@rspsystems.com>
2024-03-11 15:11:08 +0100

    Mark isClassConstructor() as stable

--- packages/support/src/reflections/isClassConstructor.ts
+++ packages/support/src/reflections/isClassConstructor.ts
@@ -1,30 +1,26 @@
 /**
- * **WARNING**: _Method is currently unsafe to use! It is subject for breaking changes or possible removal._
- * 
- * Determine if given argument is a class constructor
+ * Determine if given value is a class constructor (es6)
  * 
  * @see https://github.com/caitp/TC39-Proposals/blob/trunk/tc39-reflect-isconstructor-iscallable.md
  * 
- * @param {unknown} argument
+ * @param {unknown} value
  * 
  * @return {boolean}
  */
-export function isClassConstructor(argument: unknown): boolean
+export function isClassConstructor(value: unknown): boolean
 {
-    // TODO: WARNING: Method is currently unsafe to use! It is subject for breaking changes or possible removal.
-    
     // Source is heavily inspired by Denis Pushkarev's Core-js implementation of
     // Function.isCallable / Function.isConstructor, License MIT
     // @see https://github.com/zloirock/core-js#function-iscallable-isconstructor-
     
-    if (typeof argument != 'function') {
+    if (typeof value != 'function') {
         return false;
     }
 
     try {
         // Obtain a small part of the argument's string representation, to avoid
         // too large string from being processed by regex.
-        const source: string = argument.toString().slice(0, 25);
+        const source: string = value.toString().slice(0, 25);
 
         // Determine if source starts with "class".
         return new RegExp(/^\s*class\b/).test(source);
