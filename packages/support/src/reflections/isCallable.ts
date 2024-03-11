0ada395abda0ffb8b04c89b21892f3afe144dc9e
alin <alin@rspsystems.com>
2024-03-11 15:22:26 +0100

    Mark isCallable() as stable
    Until proposal gets through (hopefully), then this implementation will have to do.

--- packages/support/src/reflections/isCallable.ts
+++ packages/support/src/reflections/isCallable.ts
@@ -1,24 +1,20 @@
 import { isClassConstructor } from "./isClassConstructor";
 
 /**
- * **WARNING**: _Method is currently unsafe to use! It is subject for breaking changes or possible removal._
- * 
- * Determine if given argument is callable, but is not a class constructor
+ * Determine if given argument is callable, but is not a class constructor (es6 style)
  *
  * @see {isClassConstructor}
  * @see https://github.com/caitp/TC39-Proposals/blob/trunk/tc39-reflect-isconstructor-iscallable.md
  * 
- * @param {unknown} argument
+ * @param {unknown} value
  * 
  * @return {boolean}
  */
-export function isCallable(argument: unknown): boolean
+export function isCallable(value: unknown): boolean
 {
-    // TODO: WARNING: Method is currently unsafe to use! It is subject for breaking changes or possible removal.
-    
     // Source is heavily inspired by Denis Pushkarev's Core-js implementation of
     // Function.isCallable / Function.isConstructor, License MIT
     // @see https://github.com/zloirock/core-js#function-iscallable-isconstructor-
 
-    return typeof argument == 'function' && !isClassConstructor(argument);
+    return typeof value == 'function' && !isClassConstructor(value);
 }
