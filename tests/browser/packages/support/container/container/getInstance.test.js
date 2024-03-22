9365bbf0f8ec6596d2749196f99117accfad42ab
alin <alin@rspsystems.com>
2024-03-22 15:12:58 +0100

    Add test for container's singleton instance

---
+++ tests/browser/packages/support/container/container/getInstance.test.js
@@ -0,0 +1,18 @@
+import { Container } from "@aedart/container";
+
+describe('@aedart/support/container', () => {
+    describe('getInstance', () => {
+
+        it('returns singleton instance', () => {
+            const a = Container.getInstance();
+            const b = Container.getInstance();
+            
+            expect(b)
+                .withContext('Incorrect container instance returned')
+                .toBe(a);
+
+            Container.setInstance(null);
+        });
+        
+    });
+});
