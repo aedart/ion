dbd844566ef83055983693ea80d28ed0a0cf1fe8
alin <alin@rspsystems.com>
2024-03-22 11:40:11 +0100

    Add Binding Entry class

---
+++ packages/container/src/BindingEntry.ts
@@ -0,0 +1,151 @@
+import type {
+    Binding,
+    FactoryCallback,
+    Identifier
+} from "@aedart/contracts/container";
+import type { Constructor } from "@aedart/contracts";
+import { isConstructor } from "@aedart/support/reflections";
+
+/**
+ * Binding Entry
+ */
+export default class BindingEntry<T = any> implements Binding<T>
+{
+    /**
+     * This binding's identifier
+     *
+     * @type {Identifier}
+     *
+     * @readonly
+     */
+    readonly #identifier: Identifier;
+
+    /**
+     * The bound value to be resolved by a service container
+     *
+     * @template T = any
+     *
+     * @type {FactoryCallback<T> | Constructor<T>}
+     *
+     * @readonly
+     */
+    readonly #value: FactoryCallback<T> | Constructor<T>;
+
+    /**
+     * Shared state of resolved value
+     *
+     * @type {boolean} If `true`, then service container must register resolved
+     *                 value as a singleton.
+     *
+     * @readonly
+     */
+    readonly #shared: boolean;
+
+    /**
+     * State, whether value is a factory callback or not
+     * 
+     * @type {boolean|null}
+     * 
+     * @private
+     */
+    #isFactoryCallback: boolean|null = null;
+
+    /**
+     * State, whether value is a constructor or not
+     * 
+     * @type {boolean|null}
+     * 
+     * @private
+     */
+    #isConstructor: boolean|null = null;
+    
+    /**
+     * Create new Binding Entry instance
+     * 
+     * @template T = any
+     * 
+     * @param {Identifier} identifier
+     * @param {FactoryCallback<T> | Constructor<T>} value
+     * @param {boolean} [shared=false]
+     */
+    constructor(identifier: Identifier, value: FactoryCallback<T> | Constructor<T>, shared: boolean = false)
+    {
+        this.#identifier = identifier;
+        this.#value = value;
+        this.#shared = shared;
+
+        this.resolveIsConstructorOrFactoryCallback();
+    }
+    
+    /**
+     * This binding's identifier
+     *
+     * @type {Identifier}
+     *
+     * @readonly
+     */
+    get identifier(): Identifier
+    {
+        return this.#identifier;
+    }
+
+    /**
+     * The bound value to be resolved by a service container
+     *
+     * @template T = any
+     *
+     * @type {FactoryCallback<T> | Constructor<T>}
+     *
+     * @readonly
+     */
+    get value(): FactoryCallback<T> | Constructor<T>
+    {
+        return this.#value;
+    }
+
+    /**
+     * Shared state of resolved value
+     *
+     * @type {boolean} If `true`, then service container must register resolved
+     *                 value as a singleton.
+     *
+     * @readonly
+     */
+    get shared(): boolean
+    {
+        return this.#shared;
+    }
+    
+    /**
+     * Determine if bound value is a {@link FactoryCallback}
+     *
+     * @returns {boolean}
+     */
+    isFactoryCallback(): boolean
+    {
+        return this.#isFactoryCallback as boolean;
+    }
+
+    /**
+     * Determine if bound value is a {@link Constructor}
+     *
+     * @returns {boolean}
+     */
+    isConstructor(): boolean
+    {
+        return this.#isConstructor as boolean;
+    }
+
+    /**
+     * Resolves the "is constructor" or "is factory callback" values
+     * 
+     * @return {void}
+     * 
+     * @protected
+     */
+    protected resolveIsConstructorOrFactoryCallback(): void
+    {
+        this.#isConstructor = isConstructor(this.#value);
+        this.#isFactoryCallback = !this.#isConstructor; 
+    }
+}
