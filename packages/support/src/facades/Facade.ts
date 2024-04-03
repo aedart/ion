6c21cf687c30dce9764b2e67742f9be7f76675f9
alin <alin@rspsystems.com>
2024-04-03 16:13:39 +0200

    Add Facade abstraction

---
+++ packages/support/src/facades/Facade.ts
@@ -0,0 +1,321 @@
+import type { Container, Identifier } from "@aedart/contracts/container";
+import type { Callback } from "@aedart/contracts";
+import { isset } from "@aedart/support/misc";
+import { AbstractClassError, LogicalError } from "@aedart/support/exceptions";
+
+/**
+ * Facade
+ * 
+ * Adaptation of Laravel's Facade abstraction.
+ * 
+ * @see https://github.com/laravel/framework/blob/master/src/Illuminate/Support/Facades/Facade.php
+ * 
+ * @abstract
+ */
+export default abstract class Facade
+{
+    /**
+     * The facade's service container instance
+     *
+     * @type {Container|undefined}
+     *
+     * @private
+     * @static
+     */
+    static #container: Container | undefined = undefined;
+
+    /**
+     * Resolved instances
+     * 
+     * @type {Map<Identifier, any>}
+     * 
+     * @private
+     * @static
+     */
+    static #resolved: Map<
+        Identifier,
+        any
+    > = new Map();
+
+    /**
+     * Registered object spies
+     * 
+     * @type {Set<Identifier>}
+     * 
+     * @private
+     * @static
+     */
+    static #spies: Set<Identifier> = new Set();
+    
+    /**
+     * Facade Constructor
+     * 
+     * @protected
+     */
+    protected constructor() {
+        /* @ts-expect-error TS2345 Facade constructor is abstract */
+        throw new AbstractClassError(this);
+    }
+    
+    /**
+     * Returns identifier to be used for resolving facade's instance
+     * 
+     * @return {Identifier}
+     * 
+     * @abstract
+     * 
+     * @static
+     */
+    public static getIdentifier(): Identifier
+    {
+        throw new LogicalError('Facade does not implement the getAccessor() method');
+    }
+
+    /**
+     * Obtain the facade's underlying resolved object instance, or object "spy"
+     *
+     * @return {any}
+     *
+     * @throws {NotFoundException}
+     * @throws {ContainerException}
+     * @throws {LogicalError}
+     * 
+     * @abstract
+     *
+     * @static
+     */
+    public static obtain()
+    {
+        throw new LogicalError('Facade does not implement the get() method');
+    }
+
+    /**
+     * Register a "spy" (e.g. object mock) for this facade's identifier
+     * 
+     * @param {Callback} callback Callback to be used for creating some kind of object spy
+     *                   or mock, with appropriate configuration and expectations for testing
+     *                   purposes.
+     *                   
+     * @return {ReturnType<typeof callback>}
+     * 
+     * @static
+     */
+    public static spy(callback: Callback): ReturnType<typeof callback>
+    {
+        const identifier = this.getIdentifier();
+        const spy = callback(identifier);
+
+        this.swap(spy);
+        
+        this.#spies.add(identifier);
+        
+        return spy;
+    }
+
+    /**
+     * Determine if a spy has been registered for this facade's identifier
+     * 
+     * @return {boolean}
+     * 
+     * @static
+     */
+    public static isSpy(): boolean
+    {
+        return this.#spies.has(this.getIdentifier());
+    }
+
+    /**
+     * Removes registered spy for this facade's identifier
+     * 
+     * **Warning**: _Method does NOT perform any actual "spy" cleanup logic.
+     * It only removes the reference to the "spy" object._
+     * 
+     * @return {boolean}
+     * 
+     * @static
+     */
+    public static forgetSpy(): boolean
+    {
+        const identifier = this.getIdentifier();
+        
+        return this.forgetResolved(identifier)
+            && this.#spies.delete(identifier);
+    }
+
+    /**
+     * Removes all registered spies
+     * 
+     * @return {void}
+     * 
+     * @static
+     */
+    public static forgetAllSpies(): void
+    {
+        for (const identifier of this.#spies) {
+            this.forgetResolved(identifier);
+        }
+
+        this.#spies.clear();
+    }
+    
+    /**
+     * Swap the facade's underlying instance
+     * 
+     * @param {any} instance
+     * 
+     * @return {void}
+     * 
+     * @static
+     */
+    public static swap(instance: any): void
+    {
+        const identifier = this.getIdentifier();
+        
+        this.#resolved.set(identifier, instance);
+        
+        if (this.hasContainer()) {
+            (this.getContainer() as Container).instance(identifier, instance);
+        }
+    }
+    
+    /**
+     * Set this facade's service container instance
+     *
+     * @param {Container | undefined} container
+     *
+     * @return {this}
+     *
+     * @static
+     */
+    public static setContainer(container: Container | undefined): Facade
+    {
+        this.#container = container;
+
+        return this;
+    }
+
+    /**
+     * Get this facade's service container instance
+     *
+     * @return {Container | undefined}
+     *
+     * @static
+     */
+    public static getContainer(): Container | undefined
+    {
+        return this.#container;
+    }
+
+    /**
+     * Determine if facade's has a service container instance set
+     *
+     * @return {boolean}
+     *
+     * @static
+     */
+    public static hasContainer(): boolean
+    {
+        return isset(this.#container);
+    }
+
+    /**
+     * Removes this facade's service container instance
+     *
+     * @return {this}
+     *
+     * @static
+     */
+    public static forgetContainer(): Facade
+    {
+        this.#container = undefined;
+
+        return this;
+    }
+
+    /**
+     * Determine if resolved facade instance exists
+     * 
+     * @param {Identifier} identifier
+     * 
+     * @reurn {boolean}
+     * 
+     * @static
+     */
+    public static hasResolved(identifier: Identifier): boolean
+    {
+        return this.#resolved.has(identifier);
+    }
+    
+    /**
+     * Forget resolved facade instance
+     * 
+     * @param {Identifier} identifier
+     * 
+     * @return {boolean}
+     * 
+     * @static
+     */
+    public static forgetResolved(identifier: Identifier): boolean
+    {
+        return this.#resolved.delete(identifier);
+    }
+
+    /**
+     * Forget all resolved facade instances
+     * 
+     * @return {void}
+     * 
+     * @static
+     */
+    public static forgetAllResolved(): void
+    {
+        this.#resolved.clear();
+    }
+
+    /**
+     * Destroy all resolved facade instances and forget the service container
+     * 
+     * @return {void}
+     */
+    public static destroyFacades(): void
+    {
+        this.forgetAllSpies();
+        this.forgetAllResolved();
+        this.forgetContainer();
+    }
+
+    /**
+     * Get the facade's underlying object instance or object spy
+     *
+     * @template T = any
+     *
+     * @param {Identifier} identifier
+     * 
+     * @return {T}
+     *
+     * @throws {NotFoundException}
+     * @throws {ContainerException}
+     * @throws {LogicalError}
+     * 
+     * @protected
+     * 
+     * @static
+     */
+    protected static resolve<
+        T = any /* eslint-disable-line @typescript-eslint/no-explicit-any */
+    >(identifier: Identifier): T
+    {
+        if (!this.hasContainer()) {
+            throw new LogicalError('Facade has no service container instance set');
+        }
+        
+        if (this.hasResolved(identifier)) {
+            return this.#resolved.get(identifier) as T;
+        }
+
+        const resolved = (this.getContainer() as Container).make<T>(identifier);
+        this.#resolved.set(identifier, resolved);
+
+        return resolved;
+    }
+}
