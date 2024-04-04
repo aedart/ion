8e3d53db1d7649401326e4e49246ffb2b5d682ad
alin <alin@rspsystems.com>
2024-04-04 12:52:30 +0200

    Export facade related types aliases

---
+++ packages/contracts/src/support/facades/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Support Facades identifier
+ *
+ * @type {Symbol}
+ */
+export const SUPPORT_FACADES: unique symbol = Symbol('@aedart/contracts/support/facades');
+
+export type * from './types';
