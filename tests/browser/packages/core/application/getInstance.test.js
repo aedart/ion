d31f5b24493878955a98e61d701897873bd2123a
Alin Eugen Deac <aedart@gmail.com>
2024-04-12 20:28:06 +0200

    Add test for Core Application singleton instance

---
+++ tests/browser/packages/core/application/getInstance.test.js
@@ -0,0 +1,27 @@
+import { Container } from "@aedart/container";
+import { Application } from "@aedart/core";
+
+describe('@aedart/core', () => {
+    describe('getInstance', () => {
+
+        it('returns singleton instance', () => {
+            const a = Application.getInstance();
+            const b = Application.getInstance();
+
+            expect(b)
+                .withContext('Incorrect container instance returned')
+                .toBe(a);
+
+            expect(a)
+                .withContext('instance should be instance of a Container')
+                .toBeInstanceOf(Container);
+
+            expect(a)
+                .withContext('instance should be instance of a Application')
+                .toBeInstanceOf(Application);
+            
+            Application.setInstance(null);
+        });
+
+    });
+});
