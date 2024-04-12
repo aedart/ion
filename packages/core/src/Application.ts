0a6d97ba9cedb968bf92dfa3ee56d1442b32bc5f
Alin Eugen Deac <aedart@gmail.com>
2024-04-12 19:40:01 +0200

    Add Core Application

---
+++ packages/core/src/Application.ts
@@ -0,0 +1,12 @@
+import type {
+    Application as ApplicationContract
+} from "@aedart/contracts/core";
+import { Container } from "@aedart/container";
+
+/**
+ * Core Application
+ */
+export default class Application extends Container implements ApplicationContract
+{
+    
+}
