47c3b6072c5373bc26fcaa5d61137b93f48fd96c
Alin Eugen Deac <aedart@gmail.com>
2024-04-12 20:27:21 +0200

    Overwrite getInstance() and setInstance()

--- packages/core/src/Application.ts
+++ packages/core/src/Application.ts
@@ -8,5 +8,25 @@ import { Container } from "@aedart/container";
  */
 export default class Application extends Container implements ApplicationContract
 {
-    
+    /**
+     * Returns the singleton instance of the core application
+     *
+     * @return {ApplicationContract|this}
+     */
+    public static getInstance(): ApplicationContract
+    {
+        return super.getInstance() as ApplicationContract;
+    }
+
+    /**
+     * Set the singleton instance of the core application
+     *
+     * @param {ApplicationContract | null} [container]
+     *
+     * @return {ApplicationContract | null}
+     */
+    public static setInstance(container: ApplicationContract | null = null): ApplicationContract | null
+    {
+        return super.setInstance(container);
+    }
 }
