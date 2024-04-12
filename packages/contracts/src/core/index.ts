c47727e9e061235a7fa4b2f6b4b0b51662f61550
Alin Eugen Deac <aedart@gmail.com>
2024-04-12 19:16:23 +0200

    Export Core Application interface
    Also, added an alias for the "Core" identifier

--- packages/contracts/src/core/index.ts
+++ packages/contracts/src/core/index.ts
@@ -4,3 +4,17 @@
  * @type {Symbol}
  */
 export const CORE: unique symbol = Symbol('@aedart/contracts/core');
+
+/**
+ * Core Application identifier
+ * 
+ * _Alias for the {@link CORE} identifier_
+ * 
+ * @type {typeof CORE}
+ */
+export const APPLICATION: symbol = CORE;
+
+import Application from "./Application";
+export {
+    type Application,
+}
