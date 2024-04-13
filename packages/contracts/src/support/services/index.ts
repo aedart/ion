9d04a7939bfac4c149c634f76ee59bdef69273f4
Alin Eugen Deac <aedart@gmail.com>
2024-04-13 11:11:30 +0200

    Export ServiceProvider interface

--- packages/contracts/src/support/services/index.ts
+++ packages/contracts/src/support/services/index.ts
@@ -4,3 +4,8 @@
  * @type {Symbol}
  */
 export const SUPPORT_SERVICES: unique symbol = Symbol('@aedart/contracts/support/services');
+
+import ServiceProvider from "./ServiceProvider";
+export {
+    type ServiceProvider
+}
