5ebd22b8205af95cb8ec9832f3217a65268ab6ed
Alin Eugen Deac <aedart@gmail.com>
2024-04-13 11:11:21 +0200

    Add ServiceProvider interface (incomplete)
    For now, we only need the register functionality. Later, a boot method must be added.

---
+++ packages/contracts/src/support/services/ServiceProvider.ts
@@ -0,0 +1,16 @@
+/**
+ * Service Provider
+ * 
+ * Adaptation of Laravel's Service Provider
+ * 
+ * @see https://github.com/laravel/framework/blob/11.x/src/Illuminate/Support/ServiceProvider.php
+ */
+export default interface ServiceProvider
+{
+    /**
+     * Register application services
+     * 
+     * @return {void}
+     */
+    register(): void;
+}
