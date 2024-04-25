c63ae432c46182c8883fb95396ac3fd19a8590db
alin <alin@rspsystems.com>
2024-04-25 14:19:03 +0200

    Add tests for register()
    Boot logic not yet tested.

---
+++ tests/browser/packages/support/services/registrar/register.test.js
@@ -0,0 +1,46 @@
+import { ServiceProvider, ServiceRegistrar, RegistrationError } from "@aedart/support/services";
+import { Application } from "@aedart/core";
+
+describe('@aedart/support/services', () => {
+    describe('Registrar', () => {
+        describe('register()', () => {
+
+            it('can register service provider (constructor)', async () => {
+                class A extends ServiceProvider {}
+
+                const registrar = new ServiceRegistrar(new Application());
+                
+                // ------------------------------------------------------------------------- //
+                
+                const result = await registrar.register(A, false);
+                
+                expect(result)
+                    .toBeTrue();
+
+                expect(registrar.isRegistered(A))
+                    .withContext('Service provider is not marked as registered')
+                    .toBeTrue();
+            });
+
+            it('can register service provider (instance)', async () => {
+                class A extends ServiceProvider {}
+
+                const app = new Application();
+                const registrar = new ServiceRegistrar(app);
+
+                const serviceProvider = new A(app); 
+                
+                // ------------------------------------------------------------------------- //
+                
+                const result = await registrar.register(serviceProvider, false);
+
+                expect(result)
+                    .toBeTrue();
+
+                expect(registrar.isRegistered(serviceProvider))
+                    .withContext('Service provider is not marked as registered')
+                    .toBeTrue();
+            });
+        });
+    });
+});
