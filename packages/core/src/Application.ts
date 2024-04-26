8cd2eb586ecc947dd4bc56d0ed3f6ca338b86703
alin <alin@rspsystems.com>
2024-04-26 15:30:00 +0200

    Implement interface methods (incomplete)

--- packages/core/src/Application.ts
+++ packages/core/src/Application.ts
@@ -1,9 +1,22 @@
+import {
+    Callback,
+    ClassMethodReference
+} from "@aedart/contracts";
 import type {
-    Application as ApplicationContract
+    Application as ApplicationContract,
+    BootstrapperConstructor,
+    BootCallback,
+    TerminationCallback
 } from "@aedart/contracts/core";
+import { CallbackWrapper } from "@aedart/contracts/support";
+import { Container } from "@aedart/container";
 import { CORE } from "@aedart/contracts/core";
 import { CONTAINER } from "@aedart/contracts/container";
-import { Container } from "@aedart/container";
+import type {
+    Registrar,
+    ServiceProvider,
+    ServiceProviderConstructor
+} from "@aedart/contracts/support/services";
 import { version } from "../package.json";
 
 /**
@@ -53,5 +66,184 @@ export default class Application extends Container implements ApplicationContrac
     get version(): string
     {
         return version;
+    }
+
+    /**
+     * The Service Registrar used by this application
+     *
+     * @type {Registrar}
+     */
+    public get registrar(): Registrar
+    {
+        // TODO: Implement this...
+        return {} as Registrar;
+    }
+
+    // TODO: Application Environment ???
+
+    /**
+     * Register a service provider
+     *
+     * @param {ServiceProvider | ServiceProviderConstructor} provider
+     *
+     * @returns {Promise<boolean>} False if already registered
+     *
+     * @async
+     */
+    public async register(provider: ServiceProvider | ServiceProviderConstructor): Promise<boolean>
+    {
+        // TODO: Implement this...
+        return Promise.resolve(false);
+    }
+
+    /**
+     * Register multiple service providers
+     *
+     * **Note**: _Method skips providers that have already been registered!_
+     *
+     * @param {(ServiceProvider | ServiceProviderConstructor)[]} providers
+     *
+     * @returns {Promise<boolean>}
+     *
+     * @async
+     */
+    public async registerMultiple(providers: (ServiceProvider | ServiceProviderConstructor)[]): Promise<boolean>
+    {
+        // TODO: Implement this...
+        return Promise.resolve(false);
+    }
+
+    /**
+     * Run given application bootstrappers
+     *
+     * **Note**: _Method does nothing if application has already
+     * been bootstrapped._
+     *
+     * @see hasBeenBootstrapped
+     *
+     * @param {BootstrapperConstructor[]} bootstrappers
+     *
+     * @return {this}
+     */
+    public bootstrapWith(bootstrappers: BootstrapperConstructor[]): this
+    {
+        // TODO: Implement this...
+        return this;
+    }
+
+    /**
+     * Determine if application has been bootstrapped
+     *
+     * @return {boolean}
+     */
+    public hasBeenBootstrapped(): boolean
+    {
+        // TODO: Implement this...
+        return false;
+    }
+
+    /**
+     * Boot this application's service providers
+     *
+     * @returns {Promise<boolean>}
+     *
+     * @async
+     */
+    public async boot(): Promise<boolean>
+    {
+        // TODO: Implement this...
+        return Promise.resolve(false);
+    }
+
+    /**
+     * Determine if application has booted
+     *
+     * @see boot
+     *
+     * @return {boolean}
+     */
+    public hasBooted(): boolean
+    {
+        // TODO: Implement this...
+        return false;
+    }
+
+    /**
+     * Register a callback to be invoked before this application boots
+     *
+     * @param {BootCallback} callback
+     *
+     * @return {this}
+     */
+    public before(callback: BootCallback): this
+    {
+        // TODO: Implement this...
+        return this;
+    }
+
+    /**
+     * Register a callback to be invoked after this application has booted
+     *
+     * @param {BootCallback} callback
+     *
+     * @return {this}
+     */
+    public after(callback: BootCallback): this
+    {
+        // TODO: Implement this...
+        return this;
+    }
+
+    /**
+     * Run this application
+     *
+     * **Note**: _Method will bootstrap application's bootstrappers and boot
+     * the registered service providers. It does nothing if the application is
+     * already running._
+     *
+     * @param {Callback | CallbackWrapper | ClassMethodReference} [callback] Invoked after bootstrapping and booting has completed
+     *
+     * @return {Promise<boolean>}
+     *
+     * @async
+     */
+    public async run(callback?: Callback | CallbackWrapper | ClassMethodReference): Promise<boolean>
+    {
+        // TODO: Implement this...
+        return Promise.resolve(false);
+    }
+
+    /**
+     * Determine if application is running
+     *
+     * @return {boolean}
+     */
+    public isRunning(): boolean
+    {
+        // TODO: Implement this...
+        return false;
+    }
+
+    /**
+     * Terminate this application
+     *
+     * @return {void}
+     */
+    public terminate(): void
+    {
+        // TODO: Implement this...
+    }
+
+    /**
+     * Register a callback to be invoked when the application is terminating
+     *
+     * @param {TerminationCallback} callback
+     *
+     * @return {this}
+     */
+    public terminating(callback: TerminationCallback): this
+    {
+        // TODO: Implement this...
+        return this;
     }
 }
