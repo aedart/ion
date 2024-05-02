37c9fbf0148a9aded054fed981b0c1ffb3f850bd
alin <alin@rspsystems.com>
2024-05-02 15:24:59 +0200

    Change webpack config, inject environment variables into _ENV token

--- shared/tests/webpack.config.js
+++ shared/tests/webpack.config.js
@@ -1,4 +1,6 @@
-const fs = require('fs');
+const fs = require('node:fs');
+const dotEnv = require('dotenv');
+const webpack = require('webpack');
 
 /*****************************************************************
  * Load babel presents...
@@ -8,6 +10,15 @@ const babel = JSON.parse(
     fs.readFileSync('babel.config.json', 'utf8')
 );
 console.debug('Babel presets', babel.presets);
+
+/*****************************************************************
+ * Load .env content
+ ****************************************************************/
+
+const ENV = dotEnv.parse(
+    fs.readFileSync('.env', 'utf8')
+);
+// console.debug('.env', ENV);
 
 /*****************************************************************
  * Webpack Configuration (for tests)
@@ -31,6 +42,13 @@ module.exports = {
                     plugins: babel.plugins
                 }
             }
-        ]
-    }
+        ],
+    },
+    plugins: [
+        
+        // Inject environment variables into token 
+        new webpack.DefinePlugin({
+            _ENV: JSON.stringify(ENV)
+        })
+    ]
 };
