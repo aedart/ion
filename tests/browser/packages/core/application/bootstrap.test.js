9e1287a29a6fc50071706bf19cfd08ebc3575ec5
alin <alin@rspsystems.com>
2024-05-29 15:39:59 +0200

    Add tests for application bootstrapping logic

---
+++ tests/browser/packages/core/application/bootstrap.test.js
@@ -0,0 +1,51 @@
+import { Application } from "@aedart/core";
+
+describe('@aedart/core', () => {
+    describe('bootstrap', () => {
+
+        it('application can be bootstrapped', () => {
+            
+            const bootstrapped = [];
+            
+            class A {
+                bootstrap() { bootstrapped.push('a'); }
+            }
+
+            class B {
+                bootstrap() { bootstrapped.push('b'); }
+            }
+
+            class C {
+                bootstrap() { bootstrapped.push('c'); }
+            }
+            
+            const app = new Application();
+
+            // ------------------------------------------------------------------------------- //
+            
+            const before = app.hasBeenBootstrapped();
+            
+            app.bootstrapWith([ A, B, C ]);
+            
+            const after = app.hasBeenBootstrapped();
+
+            // ------------------------------------------------------------------------------- //
+            
+            expect(before)
+                .withContext('Application should NOT have been bootstrapped (before)')
+                .toBeFalse();
+
+            expect(after)
+                .withContext('Application should have been bootstrapped (after)')
+                .toBeTrue();
+            
+            expect(bootstrapped.length)
+                .withContext('Incorrect amount of bootstrappers invoked')
+                .toBe(3);
+            
+            expect(bootstrapped)
+                .withContext('Incorrect bootstrappers invoked')
+                .toEqual([ 'a', 'b', 'c' ]);
+        });
+    });
+});
