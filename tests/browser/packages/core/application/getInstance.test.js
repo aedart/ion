d16a4d10aa7993cf3ac3aa696bab0826315eb68c
Alin Eugen Deac <aedart@gmail.com>
2024-09-29 16:58:44 +0200

    Adapt instance tests to assert core bootstrappers setup
    This too must be changed again (see previous commit for details).

--- tests/browser/packages/core/application/getInstance.test.js
+++ tests/browser/packages/core/application/getInstance.test.js
@@ -1,3 +1,5 @@
+import { CORE } from "@aedart/contracts/core";
+import { CONTAINER } from "@aedart/contracts/container";
 import { Container } from "@aedart/container";
 import { Application } from "@aedart/core";
 
@@ -7,11 +9,21 @@ describe('@aedart/core', () => {
         it('returns singleton instance', () => {
             const a = Application.getInstance();
             const b = Application.getInstance();
+            const c = a.get(CONTAINER);
+            const d = a.get(CORE);
 
             expect(b)
-                .withContext('Incorrect container instance returned')
+                .withContext('Incorrect container instance returned (for b)')
                 .toBe(a);
 
+            expect(c)
+                .withContext('Incorrect container instance returned (for c)')
+                .toBe(b);
+
+            expect(d)
+                .withContext('Incorrect container instance returned (for d)')
+                .toBe(c);
+            
             expect(a)
                 .withContext('instance should be instance of a Container')
                 .toBeInstanceOf(Container);
@@ -20,7 +32,21 @@ describe('@aedart/core', () => {
                 .withContext('instance should be instance of a Application')
                 .toBeInstanceOf(Application);
             
-            Application.setInstance(null);
+            a.destroy();
+            b.destroy();
+            c.destroy();
+            d.destroy();
+        });
+
+        it('singleton instance has core bootstrappers set', () => {
+            const app = Application.getInstance();
+            
+            expect(app.coreBootstrappers.length)
+                .withContext('No core bootstrappers available in singleton instance')
+                .not
+                .toBe(0);
+
+            app.destroy();
         });
 
     });
