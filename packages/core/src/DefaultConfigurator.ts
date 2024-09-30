c7303ce57a0d6563c4de7beaae12d3b760611e80
alin <alin@rspsystems.com>
2024-09-30 11:58:41 +0200

    Add default Application Configurator (incomplete)

---
+++ packages/core/src/DefaultConfigurator.ts
@@ -0,0 +1,9 @@
+import BaseConfigurator from "./configuration/BaseConfigurator";
+
+/**
+ * Default Application Configurator
+ */
+export default class DefaultConfigurator extends BaseConfigurator
+{
+    
+}
