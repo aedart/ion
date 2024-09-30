4a4869397dd80c2f133c33c60f6c652540228cff
alin <alin@rspsystems.com>
2024-09-30 11:21:16 +0200

    Add tests for "is configurator" utilities

---
+++ tests/browser/packages/core/application/configuration/isConfigurator.test.js
@@ -0,0 +1,48 @@
+import { BaseConfigurator, isConfigurator } from "@aedart/core";
+
+describe('@aedart/core', () => {
+    describe('configuration', () => {
+
+        describe('isConfigurator', () => {
+
+            it('can determine if target is an application configurator instance', () => {
+
+                class A {}
+
+                class B {
+                    for() {}
+
+                    apply() {}
+                }
+
+                class C extends B {}
+
+                class D extends BaseConfigurator {}
+
+                class E extends D {}
+
+                // ------------------------------------------------------------------------------------ //
+
+                const data = [
+                    { value: null, expected: false, name: 'Null' },
+                    { value: [], expected: false, name: 'Array' },
+                    { value: {}, expected: false, name: 'Object (empty)' },
+                    { value: new A(), expected: false, name: 'Class A (empty)' },
+
+                    { value: new B(), expected: true, name: 'Class B (custom implementation of configurator)' },
+                    { value: new C(), expected: true, name: 'Class C (inherits from custom implementation)' },
+                    { value: new D(), expected: true, name: 'Class D (inherits from BaseConfigurator abstraction)' },
+                    { value: new E(), expected: true, name: 'Class E (inherits from a base that inherits from BaseConfigurator abstraction)' },
+                ];
+
+                for (const entry of data) {
+                    expect(isConfigurator(entry.value))
+                        .withContext(`${entry.name} was expected to ${entry.expected.toString()}`)
+                        .toBe(entry.expected);
+                }
+            });
+
+        });
+
+    });
+});
---
+++ tests/browser/packages/core/application/configuration/isConfiguratorConstructor.test.js
@@ -0,0 +1,48 @@
+import { BaseConfigurator, isConfiguratorConstructor } from "@aedart/core";
+
+describe('@aedart/core', () => {
+    describe('configuration', () => {
+
+        describe('isConfiguratorConstructor', () => {
+
+            it('can determine if target is an application configurator constructor', () => {
+
+                class A {}
+
+                class B {
+                    for() {}
+
+                    apply() {}
+                }
+
+                class C extends B {}
+
+                class D extends BaseConfigurator {}
+
+                class E extends D {}
+
+                // ------------------------------------------------------------------------------------ //
+
+                const data = [
+                    { value: null, expected: false, name: 'Null' },
+                    { value: [], expected: false, name: 'Array' },
+                    { value: {}, expected: false, name: 'Object (empty)' },
+                    { value: A, expected: false, name: 'Class A (empty)' },
+
+                    { value: B, expected: true, name: 'Class B (custom implementation of configurator)' },
+                    { value: C, expected: true, name: 'Class C (inherits from custom implementation)' },
+                    { value: D, expected: true, name: 'Class D (inherits from BaseConfigurator abstraction)' },
+                    { value: E, expected: true, name: 'Class E (inherits from a base that inherits from BaseConfigurator abstraction)' },
+                ];
+
+                for (const entry of data) {
+                    expect(isConfiguratorConstructor(entry.value))
+                        .withContext(`${entry.name} was expected to ${entry.expected.toString()}`)
+                        .toBe(entry.expected);
+                }
+            });
+
+        });
+
+    });
+});
