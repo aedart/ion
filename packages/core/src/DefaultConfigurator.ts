085edc446ad2454c422f683707ff95c7f0e2b387
alin <alin@rspsystems.com>
2024-10-01 11:51:45 +0200

    Set default instances, aliases and bootstrappers
    For now, the application's instance, aliases and core bootstrappers are configured. More will be added at a later point.

--- packages/core/src/DefaultConfigurator.ts
+++ packages/core/src/DefaultConfigurator.ts
@@ -1,9 +1,34 @@
+import type { Application } from "@aedart/contracts/core";
+import {
+    CORE,
+} from "@aedart/contracts/core";
+import { CONTAINER } from "@aedart/contracts/container";
+import LoadEnvironmentVariables from "./bootstrap/LoadEnvironmentVariables";
+import SetupFacades from "./bootstrap/SetupFacades";
 import BaseConfigurator from "./configuration/BaseConfigurator";
 
 /**
  * Default Application Configurator
+ * 
+ * @see {BaseConfigurator}
  */
 export default class DefaultConfigurator extends BaseConfigurator
 {
-    
+    /**
+     * @inheritdoc
+     */
+    public before(app: Application): void
+    {
+        this
+            .withInstances([
+                [ CORE, app ]
+            ])
+            .withAliases([
+                [ CORE, CONTAINER ]
+            ])
+            .withBootstrappers([
+                LoadEnvironmentVariables,
+                SetupFacades
+            ]);
+    }
 }
