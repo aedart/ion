b7fa49085b3d1d1dd416a182ab9a2eea7f2336d2
alin <alin@rspsystems.com>
2024-10-01 15:08:01 +0200

    Add Configuration Repository (incomplete)

---
+++ packages/contracts/src/config/Repository.ts
@@ -0,0 +1,11 @@
+/**
+ * Configuration Repository
+ * 
+ * Adaptation of Laravel's configuration `Repository`.
+ * 
+ * @see https://github.com/laravel/framework/blob/master/src/Illuminate/Contracts/Config/Repository.php
+ */
+export default interface Repository
+{
+    
+}
