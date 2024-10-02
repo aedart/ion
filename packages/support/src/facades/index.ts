c227b353e99523dc82062f50885d982a19a582f6
alin <alin@rspsystems.com>
2024-10-02 15:07:24 +0200

    Export Config facade

--- packages/support/src/facades/index.ts
+++ packages/support/src/facades/index.ts
@@ -1,9 +1,11 @@
 import App from "./App";
+import Config from "./Config";
 import Container from "./Container";
 import Facade from "./Facade";
 
 export {
     App,
+    Config,
     Container,
     Facade
 }
