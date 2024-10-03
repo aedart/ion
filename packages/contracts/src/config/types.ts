ff05e1e5474b230f38a4cb9de01ba404571a074d
alin <alin@rspsystems.com>
2024-10-03 15:08:40 +0200

    Add Configuration Path and Configuration Loader Callback type aliases

--- packages/contracts/src/config/types.ts
+++ packages/contracts/src/config/types.ts
@@ -1,7 +1,15 @@
 /**
- * Configuration Items
- * 
- * A general type for describing a key-value store containing configuration items
- * for an application or component.
+ * A key-value store containing configuration items for an application or component.
  */
 export type Items = Record<PropertyKey, any>; /* eslint-disable-line @typescript-eslint/no-explicit-any */
+
+/**
+ * A [module-name](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import#modulename) from
+ * where configuration {@link Items} are to be loaded from.
+ */
+export type ConfigurationPath = string;
+
+/**
+ * Callback that is responsible for loading configuration {@link Items}.
+ */
+export type ConfigurationLoaderCallback = () => Promise<Items>;
