44376e517f71ef792ca4692aa13b7f7e9b555307
alin <alin@rspsystems.com>
2024-10-04 12:48:06 +0200

    Export Factory

--- packages/contracts/src/config/loaders/index.ts
+++ packages/contracts/src/config/loaders/index.ts
@@ -1,6 +1,8 @@
+import Factory from './Factory';
 import Loader from './Loader';
 import LoaderConstructor from "./LoaderConstructor";
 export {
+    type Factory,
     type Loader,
     type LoaderConstructor
 }
