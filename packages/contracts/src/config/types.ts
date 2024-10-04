9866f996028c56160eb9748f79eaeed4521bc055
alin <alin@rspsystems.com>
2024-10-04 09:31:58 +0200

    Replace ConfigurationPath with ModuleName type alias

--- packages/contracts/src/config/types.ts
+++ packages/contracts/src/config/types.ts
@@ -4,12 +4,6 @@
 export type Items = Record<PropertyKey, any>; /* eslint-disable-line @typescript-eslint/no-explicit-any */
 
 /**
- * A [module-name](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import#modulename) from
- * where configuration {@link Items} are to be loaded from.
- */
-export type ConfigurationPath = string;
-
-/**
  * Callback that is responsible for loading configuration {@link Items}.
  */
 export type ConfigurationLoaderCallback = () => Promise<Items>;
