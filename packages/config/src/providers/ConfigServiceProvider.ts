ff01a1828984aabb8ccf6ddd7952be5237d66cfc
alin <alin@rspsystems.com>
2024-10-04 13:18:21 +0200

    Bind configuration loader factory

--- packages/config/src/providers/ConfigServiceProvider.ts
+++ packages/config/src/providers/ConfigServiceProvider.ts
@@ -1,6 +1,8 @@
-import { CONFIG } from "@aedart/contracts/config";
+import type { Application } from "@aedart/contracts/core";
+import { CONFIG, CONFIG_LOADER_FACTORY } from "@aedart/contracts/config";
 import { ServiceProvider } from "@aedart/support/services";
 import Repository from '../Repository';
+import ConfigurationLoaderFactory from "../loaders/ConfigurationLoaderFactory";
 
 /**
  * Configuration Service Provider
@@ -14,6 +16,10 @@ export default class ConfigServiceProvider extends ServiceProvider
     {
         this.app
             .singleton(CONFIG, Repository)
-            .alias(CONFIG, 'config');
+            .alias(CONFIG, 'config')
+        
+            .singleton(CONFIG_LOADER_FACTORY, (app) => {
+                return new ConfigurationLoaderFactory(app as Application);
+            });
     }
 }
