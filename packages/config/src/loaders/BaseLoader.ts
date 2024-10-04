e05eafb13d098dbf6d34f35661d4d3248270b0fa
alin <alin@rspsystems.com>
2024-10-04 11:17:08 +0200

    Add "base" loader

---
+++ packages/config/src/loaders/BaseLoader.ts
@@ -0,0 +1,34 @@
+import type { AbstractConstructor } from "@aedart/contracts";
+import type { Items, Loader } from "@aedart/contracts/config";
+import { AbstractClassError } from "@aedart/support/exceptions";
+
+/**
+ * Base Configuration Loader
+ * 
+ * @see {Loader}
+ */
+export default abstract class BaseLoader implements Loader
+{
+    /**
+     * Create new loader instance
+     * 
+     * @param {...any} [args]
+     * 
+     * @protected
+     */
+    protected constructor(...args: any[] /* eslint-disable-line @typescript-eslint/no-unused-vars */)
+    {
+        if (new.target === BaseLoader) {
+            throw new AbstractClassError(BaseLoader as AbstractConstructor);
+        }
+    }
+    
+    /**
+     * Load configuration {@link Items}
+     *
+     * @return {Promise<Items>}
+     *
+     * @throws {import('@aedart/contracts/config').LoaderException}
+     */
+    abstract load(): Promise<Items>;
+}
