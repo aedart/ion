56d92c9dbf6159eccbfed3db3647b740791b777e
alin <alin@rspsystems.com>
2024-10-04 10:57:57 +0200

    Add identifier for config loader factory
    Also, export exceptions and loaders

--- packages/contracts/src/config/index.ts
+++ packages/contracts/src/config/index.ts
@@ -5,9 +5,18 @@
  */
 export const CONFIG: unique symbol = Symbol('@aedart/contracts/config');
 
+/**
+ * Configuration Loader Factory identifier
+ * 
+ * @type {Symbol}
+ */
+export const CONFIG_LOADER_FACTORY: unique symbol = Symbol('@aedart/contracts/config/loaders/factory');
+
 import Repository from './Repository';
 export {
     type Repository
 }
 
+export * from './loaders/index';
+export * from './exceptions/index';
 export type * from './types';
