4c4c16d405875c90c2b26505b3b1a0899acadb54
alin <alin@rspsystems.com>
2024-10-04 10:17:49 +0200

    Add Path and Source type aliases
    The idea is to also allow configuration items to be "loaded" asynchronously, due to eventual usage of env(), which might not have been set / loaded, before the configuration items have been imported!

--- packages/contracts/src/config/types.ts
+++ packages/contracts/src/config/types.ts
@@ -1,9 +1,21 @@
+import { ModuleName } from "@aedart/contracts";
+
 /**
- * A key-value store containing configuration items for an application or component.
+ * A key-value store containing configuration items for an application, its service, and or component.
  */
 export type Items = Record<PropertyKey, any>; /* eslint-disable-line @typescript-eslint/no-explicit-any */
+
+/**
+ * Path ([module-name]{@link ModuleName}) from where to load configuration {@link Items}. 
+ */
+export type Path = ModuleName;
 
 /**
  * Callback that is responsible for loading configuration {@link Items}.
  */
 export type LoaderCallback = () => Promise<Items>;
+
+/**
+ * A source that ultimately must resolve into configuration {@link Items}.
+ */
+export type Source = Items | Path | LoaderCallback;
