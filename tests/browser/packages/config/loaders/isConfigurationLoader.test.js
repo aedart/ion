5c29b4c48464884fc83308d35ed678d63e1e9b8f
alin <alin@rspsystems.com>
2024-10-04 12:11:45 +0200

    Add tests for "is config loader" utils

---
+++ tests/browser/packages/config/loaders/isConfigurationLoader.test.js
@@ -0,0 +1,46 @@
+import { BaseLoader, isConfigurationLoader } from "@aedart/config";
+
+describe('@aedart/config', () => {
+    describe('loaders', () => {
+
+        describe('isConfigurationLoader', () => {
+
+            it('can determine if target is a configuration loader instance', () => {
+
+                class A {}
+
+                class B {
+                    load(){}
+                }
+
+                class C extends B {}
+
+                class D extends BaseLoader {}
+
+                class E extends D {}
+
+                // ------------------------------------------------------------------------------------ //
+
+                const data = [
+                    { value: null, expected: false, name: 'Null' },
+                    { value: [], expected: false, name: 'Array' },
+                    { value: {}, expected: false, name: 'Object (empty)' },
+                    { value: new A(), expected: false, name: 'Class A (empty)' },
+
+                    { value: new B(), expected: true, name: 'Class B (custom implementation of loader)' },
+                    { value: new C(), expected: true, name: 'Class C (inherits from custom implementation)' },
+                    { value: new D(), expected: true, name: 'Class D (inherits from BaseLoader abstraction)' },
+                    { value: new E(), expected: true, name: 'Class E (inherits from a base that inherits from BaseLoader abstraction)' },
+                ];
+
+                for (const entry of data) {
+                    expect(isConfigurationLoader(entry.value))
+                        .withContext(`${entry.name} was expected to ${entry.expected.toString()}`)
+                        .toBe(entry.expected);
+                }
+            });
+
+        });
+
+    });
+});
---
+++ tests/browser/packages/config/loaders/isConfigurationLoaderConstructor.test.js
@@ -0,0 +1,46 @@
+import { BaseLoader, isConfigurationLoaderConstructor } from "@aedart/config";
+
+describe('@aedart/config', () => {
+    describe('loaders', () => {
+
+        describe('isConfigurationLoaderConstructor', () => {
+
+            it('can determine if target is a configuration loader constructor', () => {
+
+                class A {}
+
+                class B {
+                    load() {}
+                }
+
+                class C extends B {}
+
+                class D extends BaseLoader {}
+
+                class E extends D {}
+
+                // ------------------------------------------------------------------------------------ //
+
+                const data = [
+                    { value: null, expected: false, name: 'Null' },
+                    { value: [], expected: false, name: 'Array' },
+                    { value: {}, expected: false, name: 'Object (empty)' },
+                    { value: A, expected: false, name: 'Class A (empty)' },
+
+                    { value: B, expected: true, name: 'Class B (custom implementation of loader)' },
+                    { value: C, expected: true, name: 'Class C (inherits from custom implementation)' },
+                    { value: D, expected: true, name: 'Class D (inherits from BaseLoader abstraction)' },
+                    { value: E, expected: true, name: 'Class E (inherits from a base that inherits from BaseLoader abstraction)' },
+                ];
+
+                for (const entry of data) {
+                    expect(isConfigurationLoaderConstructor(entry.value))
+                        .withContext(`${entry.name} was expected to ${entry.expected.toString()}`)
+                        .toBe(entry.expected);
+                }
+            });
+
+        });
+
+    });
+});
