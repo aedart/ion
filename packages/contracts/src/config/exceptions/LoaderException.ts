3b01e89ffa2c721f2b9d4575f4733c1d5180c454
alin <alin@rspsystems.com>
2024-10-04 10:56:31 +0200

    Add configuration loader exception interface

---
+++ packages/contracts/src/config/exceptions/LoaderException.ts
@@ -0,0 +1,9 @@
+import { Throwable } from "@aedart/contracts/support/exceptions";
+
+/**
+ * Configuration Loader Exception
+ * 
+ * General exception to be thrown whenever a configuration load is unable to load items.
+ */
+export default interface LoaderException extends Throwable
+{}
