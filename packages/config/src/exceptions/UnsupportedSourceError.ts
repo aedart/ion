43610c139faad2631f697e3669ecac09b5355468
Alin Eugen Deac <aedart@gmail.com>
2024-10-05 17:33:26 +0200

    Change error, inherit from ResolveError

--- packages/config/src/exceptions/UnsupportedSourceError.ts
+++ packages/config/src/exceptions/UnsupportedSourceError.ts
@@ -1,13 +1,13 @@
 import type { UnsupportedSourceException } from "@aedart/contracts/config";
 import { configureCustomError } from "@aedart/support/exceptions";
-import LoaderError from "./LoaderError";
+import ResolveError from "./ResolveError";
 
 /**
  * Unsupported Configuration Source Error
  * 
  * @see {UnsupportedSourceException}
  */
-export default class UnsupportedSourceError extends LoaderError implements UnsupportedSourceException
+export default class UnsupportedSourceError extends ResolveError implements UnsupportedSourceException
 {
     /**
      * Create new Unsupported Source Error instance
