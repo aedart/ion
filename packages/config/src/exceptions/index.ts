575764c9ef3d5167e39461b040b955d0430a700d
Alin Eugen Deac <aedart@gmail.com>
2024-10-05 17:32:56 +0200

    Add Resolve Error

--- packages/config/src/exceptions/index.ts
+++ packages/config/src/exceptions/index.ts
@@ -1,6 +1,8 @@
 import LoaderError from "./LoaderError";
+import ResolveError from './ResolveError';
 import UnsupportedSourceError from "./UnsupportedSourceError";
 export {
     LoaderError,
+    ResolveError,
     UnsupportedSourceError
 }
