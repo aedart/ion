31b9f899cdefa963dd31623f8922742a95d11d11
Alin Eugen Deac <aedart@gmail.com>
2024-10-05 17:23:20 +0200

    Deprecate Loader Exception

--- packages/contracts/src/config/exceptions/LoaderException.ts
+++ packages/contracts/src/config/exceptions/LoaderException.ts
@@ -1,6 +1,8 @@
 import { Throwable } from "@aedart/contracts/support/exceptions";
 
 /**
+ * @deprecated TODO: Remove this... 
+ * 
  * Configuration Loader Exception
  * 
  * General exception to be thrown whenever a configuration load is unable to load items.
