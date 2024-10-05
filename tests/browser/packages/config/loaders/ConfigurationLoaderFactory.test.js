4ff9724e3b5dd23da39f184067297f918069ef91
Alin Eugen Deac <aedart@gmail.com>
2024-10-05 17:11:30 +0200

    Add additional dynamic import experiments
    Attempted several ways to get the dynamic import to work - some do work, but the constant issue is that the transpiled code in the @aedart/config package cannot accept a full dynamic path. A different approach is needed to resolve this issue.

--- tests/browser/packages/config/loaders/ConfigurationLoaderFactory.test.js
+++ tests/browser/packages/config/loaders/ConfigurationLoaderFactory.test.js
@@ -39,41 +39,73 @@ describe('@aedart/config', () => {
                 console.info('FILE URL # 2', (new URL('../fixtures/my-config.js', import.meta.url)).toString());
                 console.info('FILE URL # 3', (new URL('../fixtures/my-config.js', import.meta.url)).href);
                 console.info('FILE URL # 4', (new URL('../fixtures/my-config.js', import.meta.url)).pathname);
+                console.info('WEBPACK __TESTS_PATH__', __TESTS_PATH__);
                 
+                // const RELATIVE_PATH = '../fixtures/my-config.js';
+                // const FULL_PATH =  __TESTS_PATH__ + '/browser/packages/config/fixtures/my-config.js';
+                
+
+                
+                // TODO: WORKS
+                const raw = (await import('../fixtures/my-config.js')).default;
+                console.log('Raw import', raw);
+
+                // TODO: WORKS
+                const otherRaw = (await import(
+                    __TESTS_PATH__ + '/browser/packages/config/fixtures/my-config.js'
+                )).default;
+                console.log('Other Raw import', otherRaw);
+                
+                // TODO: WORKS
+                const fnA = async () => {
+                    try {
+                        return (await import('../fixtures/my-config.js')).default;    
+                    } catch (e) {
+                        console.error('fff', e.message);
+                    }
+                }
+                const viaFnA = await fnA();
+                console.log('Fn (A)', viaFnA);
+                
+                // TODO: WORKS
+                const fnB = async (p) => {
+                    try {
+                        // Acc. to webpack docs: "[...] The import() must contain at least some
+                        // information about where the module is located [...]"
+                        // @see https://webpack.js.org/api/module-methods/#dynamic-expressions-in-import
+                        return (await import(
+                            `../${p}`
+                        )).default;
+                    } catch(e) {
+                        console.error(e.message);
+                        throw e;
+                    }
+                }
+                const viaFnB = await fnB('fixtures/my-config.js');
+                console.log('Fn (B)', viaFnB);
+
+                // TODO:
+                // console.log('baseURI', document.baseURI);
+                // console.log('URL (current dir)', (new URL('./', import.meta.url)).toString());
                 class MyLoader {
                     async load(path) {
                         try {
-                            return await import(path).default;
+                            // return (await import(`./${path}.js`)).default;
+                            return (await import(`../${path}`)).default;
                         } catch (e) {
                             throw new Error(`Unable to load: ${e.message}`);
                         }
                     }
                 }
-
-                const fn = async (p) => {
-                    const c = await import(p);
-                    return c?.default;
-                }
                 
-                // TODO: WORKS
-                const raw = (await import('../fixtures/my-config.js')).default;
-                console.log('Raw import', raw);
-                
-                // TODO:
-                const viaFn = await fn('../fixtures/my-config.js');
-                console.log('Fn import', viaFn);
-
-                // const tmp = await (new MyLoader()).load('../fixtures/my-config.js');
-                // const tmp = await (new MyLoader()).load((new URL('../fixtures/my-config.js', import.meta.url)).href);
-                // const tmp = await (new MyLoader()).load(
-                //     'file://' + (new URL('../fixtures/my-config.js', import.meta.url)).href
-                // );
-                // const tmp = await (new MyLoader()).load(
-                //     (new URL('../fixtures/my-config.js', import.meta.url)).toString()
-                // );
                 const tmp = await (new MyLoader()).load(
-                    (new URL('../fixtures/my-config.js', import.meta.url))
+                    // (new URL('../fixtures/my-config.js', import.meta.url)).href
+                    // (new URL(__TESTS_PATH__ + '/browser/packages/config/fixtures/my-config.js', import.meta.url)).href
+                    'fixtures/my-config.js'
                 );
+                // const tmp = await (new MyLoader()).load(
+                //     '../fixtures/my-config'
+                // );
                 console.log('LOADED via MyLoader', tmp);
                 
                 const data = [
@@ -98,7 +130,9 @@ describe('@aedart/config', () => {
                         
                         //source: 'file://' + (new URL('../fixtures/my-config.js', import.meta.url)).href,
 
-                        source: (new URL('../fixtures/my-config.js', import.meta.url)).href,
+                        // source: (new URL('../fixtures/my-config.js', import.meta.url)).href,
+                        // source: __TESTS_PATH__ + '/browser/packages/config/fixtures/my-config.js',
+                        source: 'my-config',
                         loader: PathLoader,
                         expected: {
                             app: {
