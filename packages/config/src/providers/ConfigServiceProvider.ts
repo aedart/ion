3584079a69fc11916784f14682b93e2ed059e96b
Alin Eugen Deac <aedart@gmail.com>
2024-10-05 17:53:39 +0200

    Change provider, bind config resolver

--- packages/config/src/providers/ConfigServiceProvider.ts
+++ packages/config/src/providers/ConfigServiceProvider.ts
@@ -1,7 +1,8 @@
 import type { Application } from "@aedart/contracts/core";
-import { CONFIG, CONFIG_LOADER_FACTORY } from "@aedart/contracts/config";
+import { CONFIG, CONFIG_LOADER_FACTORY, CONFIG_RESOLVER } from "@aedart/contracts/config";
 import { ServiceProvider } from "@aedart/support/services";
 import Repository from '../Repository';
+import Resolver from '../resolvers/DefaultResolver';
 import ConfigurationLoaderFactory from "../loaders/ConfigurationLoaderFactory";
 
 /**
@@ -20,6 +21,8 @@ export default class ConfigServiceProvider extends ServiceProvider
         
             .singleton(CONFIG_LOADER_FACTORY, (app) => {
                 return new ConfigurationLoaderFactory(app as Application);
-            });
+            })
+        
+            .singleton(CONFIG_RESOLVER, Resolver);
     }
 }
