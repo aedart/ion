8378db1265bcdaa88f131b5a7db5bfece20a552f
Alin Eugen Deac <aedart@gmail.com>
2024-10-05 17:25:55 +0200

    Add Configuration Resolve Exception interface

--- packages/contracts/src/config/exceptions/index.ts
+++ packages/contracts/src/config/exceptions/index.ts
@@ -1,6 +1,8 @@
 import LoaderException from "./LoaderException";
+import ResolveException from "./ResolveException";
 import UnsupportedSourceException from "./UnsupportedSourceException";
 export {
     type LoaderException,
+    type ResolveException,
     type UnsupportedSourceException
 }
