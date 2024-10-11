5b61dbddc3c683141fd7daf1f9b1bfc8bef89997
alin <alin@rspsystems.com>
2024-10-11 13:57:31 +0200

    Add example command for dealing with .env files

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -2,17 +2,29 @@
 
 import { program } from "commander";
 import { VERSION } from "@aedart/cli";
+import { loadEnvFile } from "node:process"; 
 
 // TODO: Incomplete... how to obtain additional commands? Should this be wrapped?
 
 program
     .version(VERSION)
-    .description(`Ion CLI v${VERSION} in ${process.cwd()}`)
+    .description(`Ion CLI v${VERSION} in ${process.cwd()}`);
+
+program
     .command('fisk', )
     .description('Bla bla')
     .argument('name', 'More bla bla')
     .action((name) => {
         console.info('Sweet', name);
+    });
+program
+    .command('read')
+    .description('.env example')
+    .action(() => {
+        // Loads .env file from process.cwd(), unless other file is specified.
+        loadEnvFile();
+        
+        console.log(process.env);
     });
 
 // TODO: Add commands...
