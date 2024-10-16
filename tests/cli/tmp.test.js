95a38e50f8875b703f4c25e7c1d43cc866825870
Alin Eugen Deac <aedart@gmail.com>
2024-10-16 18:26:09 +0200

    Add dummy test for executing a script
    A simple test to see if application's exit code, stdout and stderr can be captured.

--- tests/cli/tmp.test.js
+++ tests/cli/tmp.test.js
@@ -1,5 +1,6 @@
 import { describe, it } from 'node:test';
 import * as assert from "node:assert";
+import { exec } from "node:child_process"
 
 /**
  * TODO: Testing nodejs - dummy file to be removed again!
@@ -10,4 +11,13 @@ describe('TODO', () => {
         assert.ok(true);
     });
 
+    it('can invoke cli application via npx', async () => {
+        return exec('npx ion', (error, stdout,stderr) => {
+            // console.log('stdout', stdout);
+            // console.error('stderr', stderr);
+            // console.log('code', error.code);
+            
+            assert.equal(error.code, 1, `Incorrect exit code: (${error.code})`);
+        });
+    });
 });
