929214216e72eb344f3c1b5c8a501d9aa4eebf97
Alin Eugen Deac <aedart@gmail.com>
2024-10-16 17:22:01 +0200

    Add dummy test
    Dummy test file to be removed again.

---
+++ tests/cli/tmp.test.js
@@ -0,0 +1,13 @@
+import { describe, it } from 'node:test';
+import * as assert from "node:assert";
+
+/**
+ * TODO: Testing nodejs - dummy file to be removed again!
+ */
+describe('TODO', () => {
+
+    it('should work', () => {
+        assert.ok(true);
+    });
+
+});
