941c3676d8a999650d022348481173f255634093
Alin Eugen Deac <aedart@gmail.com>
2024-10-20 14:02:05 +0200

    Replace dummy bin with new Cli Application (incomplete)
    The "Ion" bin will still be kept. Here, a few default development util commands will be added to the application and be offered by the package.

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -1,37 +1,43 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
-import { VERSION } from "@aedart/cli";
-import { loadEnvFile } from "node:process"; 
+// import { program } from "commander";
+// import { VERSION } from "@aedart/cli";
+// import { loadEnvFile } from "node:process"; 
+//
+// // TODO: Incomplete... how to obtain additional commands? Should this be wrapped?
+//
+// program
+//     .version(VERSION)
+//     .description(`Ion CLI v${VERSION} in ${process.cwd()}`);
+//
+// program
+//     .command('fisk', )
+//     .description('Bla bla')
+//     .argument('name', 'More bla bla')
+//     .action((name) => {
+//         console.info('Sweet', name);
+//     });
+// program
+//     .command('read')
+//     .description('.env example')
+//     .action(() => {
+//         // Loads .env file from process.cwd(), unless other file is specified.
+//         loadEnvFile();
+//        
+//         console.log(process.env);
+//     });
+//
+// // TODO: Add commands...
+//
+// // Display help automatically, when no command is requested.
+// // if (process.argv.length < 3) {
+// //     program.help();
+// // }
+//
+// await program.parseAsync();
 
-// TODO: Incomplete... how to obtain additional commands? Should this be wrapped?
+import { CliApplication } from "@aedart/cli";
 
-program
-    .version(VERSION)
-    .description(`Ion CLI v${VERSION} in ${process.cwd()}`);
+// TODO: Add default Ion commands...
 
-program
-    .command('fisk', )
-    .description('Bla bla')
-    .argument('name', 'More bla bla')
-    .action((name) => {
-        console.info('Sweet', name);
-    });
-program
-    .command('read')
-    .description('.env example')
-    .action(() => {
-        // Loads .env file from process.cwd(), unless other file is specified.
-        loadEnvFile();
-        
-        console.log(process.env);
-    });
-
-// TODO: Add commands...
-
-// Display help automatically, when no command is requested.
-// if (process.argv.length < 3) {
-//     program.help();
-// }
-
-await program.parseAsync();
+await (new CliApplication()).run();
