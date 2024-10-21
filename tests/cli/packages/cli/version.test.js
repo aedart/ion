f7f5f3435316b97529ed80bf456d4ff905ba7a97
alin <alin@rspsystems.com>
2024-10-21 14:49:25 +0200

    Add test for --version output

--- tests/cli/packages/cli/version.test.js
+++ tests/cli/packages/cli/version.test.js
@@ -6,10 +6,26 @@ describe('@aedart/cli', () => {
 
     describe('version', () => {
         
-        it('can display version', () => {
+        it('has version property', () => {
             const result = makeCliApplication().version;
 
             assert.strictEqual(typeof result, 'string', 'Cli Application does not have version defined');
+        });
+
+        it('can display version', async () => {
+            let buffer = '';
+            
+            const cli = makeCliApplication({
+                writeOut: (str) => {
+                    buffer = buffer.concat(str).trim();
+                }
+            });
+            
+            // --------------------------------------------------------------- //
+
+            await cli.run(['--version'], { from: 'user' });
+            
+            assert.strictEqual(buffer, cli.version, 'Version was not displayed in output');
         });
     });
 });
