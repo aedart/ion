49056cc2e1bf5603260c8d8718d0959c537051d7
Alin Eugen Deac <aedart@gmail.com>
2024-11-23 18:39:34 +0100

    Add type alias for Input Option static make() method params

--- packages/contracts/src/cli/types.ts
+++ packages/contracts/src/cli/types.ts
@@ -29,4 +29,56 @@ export type ArgumentParams = {
 /**
  * The value datatype for an input option
  */
-export type OptionType = 'string' | 'boolean' | 'number'; 
+export type OptionType = 'string' | 'boolean' | 'number';
+
+/**
+ * Parameters accepted by a static `make()` method for an Input Option
+ *
+ * @see {Option}
+ */
+export type OptionParams = {
+
+    /**
+     * Short description of the option
+     */
+    description: string,
+
+    /**
+     * Single character alias for the option
+     */
+    short: string | null,
+
+    /**
+     * Value datatype for the option
+     */
+    type: OptionType,
+
+    /**
+     * If this option accepts a value
+     */
+    acceptsValue: boolean,
+
+    /**
+     * If option is negatable
+     *
+     * **Note**: _If `true`, allows explicitly setting boolean
+     * option to `false` by prefixing the option name with `--no-`,
+     * e.g. `--no-print`_
+     */
+    negatable: boolean,
+
+    /**
+     * If the option is required
+     */
+    required: boolean,
+
+    /**
+     * If the option accepts multiple values
+     */
+    isArray: boolean,
+
+    /**
+     * Default value of the option
+     */
+    defaultValue?: string | number | boolean | (string|number|boolean)[] | null,
+};
