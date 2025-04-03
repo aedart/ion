e59c37109d4397e9fd9b55aa4a671693cf87ca06
alin <alin@rspsystems.com>
2025-04-03 15:57:57 +0200

    Export class blueprints

--- packages/cli/src/input/index.ts
+++ packages/cli/src/input/index.ts
@@ -1,6 +1,10 @@
 import Argument from "./Argument";
 import Option from './Option';
+
 export {
     Argument,
     Option
 }
+
+export * from './ArgumentClassBlueprint';
+export * from './OptionClassBlueprint';
