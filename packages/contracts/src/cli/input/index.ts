b167c8e91e2f1be286abe69ff36f747db3f1377b
alin <alin@rspsystems.com>
2025-04-03 14:18:26 +0200

    Export Input Definition

--- packages/contracts/src/cli/input/index.ts
+++ packages/contracts/src/cli/input/index.ts
@@ -1,9 +1,11 @@
 import Argument from "./Argument";
 import Option from "./Option";
+import Definition from "./Definition";
 import OptionType from "./OptionType";
 
 export {
     type Argument,
     type Option,
+    type Definition,
     OptionType
 }
