2ba551e875693baa2c8fc6a74e7915f459bda036
alin <alin@rspsystems.com>
2025-04-07 10:16:03 +0200

    Export Input interface

--- packages/contracts/src/cli/input/index.ts
+++ packages/contracts/src/cli/input/index.ts
@@ -1,12 +1,14 @@
 import Argument from "./Argument";
 import Option from "./Option";
 import Definition from "./Definition";
+import Input from "./Input";
 import ValueMode from "./ValueMode";
 
 export {
     type Argument,
     type Option,
     type Definition,
+    type Input,
 
     ValueMode
 }
