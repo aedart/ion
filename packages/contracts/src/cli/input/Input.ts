48e25aec717f5b00b726c23ef3b71e3354583f19
alin <alin@rspsystems.com>
2025-04-07 10:37:29 +0200

    Fix JSDoc

--- packages/contracts/src/cli/input/Input.ts
+++ packages/contracts/src/cli/input/Input.ts
@@ -6,7 +6,7 @@ import Definition from "./Definition";
  * This component is an adaptation of Symfony Console's `InputInterface` - Copyright Fabien Potencier
  * 2004-present, MIT License.
  * 
- * https://github.com/symfony/console/blob/7.1/Input/InputInterface.php
+ * @see https://github.com/symfony/console/blob/7.1/Input/InputInterface.php
  */
 export default interface Input
 {
