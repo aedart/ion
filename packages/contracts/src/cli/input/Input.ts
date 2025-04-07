fa08d5aa4e90054a2b5fcada74dfebda329e6f77
alin <alin@rspsystems.com>
2025-04-07 10:30:49 +0200

    Add API methods for Input methods
    More might be added at a later point.

--- packages/contracts/src/cli/input/Input.ts
+++ packages/contracts/src/cli/input/Input.ts
@@ -1,3 +1,5 @@
+import Definition from "./Definition";
+
 /**
  * Input
  * 
@@ -8,5 +10,107 @@
  */
 export default interface Input
 {
-    // TODO: ...
+    /**
+     * Bind given Input Definition to this input
+     * 
+     * @param {Definition} definition
+     * 
+     * @return {this}
+     */
+    bind(definition: Definition): this;
+
+    /**
+     * Validate the input
+     * 
+     * @throws {Error}
+     * 
+     * @return {this}
+     */
+    validate(): this;
+
+    /**
+     * Set value for given argument
+     * 
+     * @param {string} name
+     * @param {string | number | boolean | (string | number | boolean)[] | null} value
+     * 
+     * @return {this}
+     *
+     * @throws {TypeError} If argument does not exist
+     */
+    setArgument(name: string, value: string | number | boolean | (string|number|boolean)[] | null): this;
+    
+    /**
+     * Get value for given argument
+     *
+     * @template T = string | number | boolean | (string|number|boolean)[] | null
+     *
+     * @param {string} name
+     *
+     * @return {T}
+     *
+     * @throws {TypeError} If argument does not exist
+     */
+    getArgument<
+        T = string | number | boolean | (string|number|boolean)[] | null
+    >(name: string): T;
+    
+    /**
+     * Determine if input argument exists
+     *
+     * @param {string} name
+     *
+     * @return {boolean}
+     */
+    hasArgument(name: string): boolean;
+    
+    /**
+     * Input arguments merged with their default values
+     * 
+     * @type {Map<string, string | number | boolean | (string | number | boolean)[] | null>}
+     */
+    get arguments(): Map<string, string | number | boolean | (string|number|boolean)[] | null>;
+
+    /**
+     * Set value for given option
+     * 
+     * @param {string} name
+     * @param {string | number | boolean | (string | number | boolean)[] | null} value
+     * 
+     * @return {this}
+     *
+     * @throws {TypeError} If option does not exist
+     */
+    setOption(name: string, value: string | number | boolean | (string|number|boolean)[] | null): this;
+
+    /**
+     * Get value for given option
+     *
+     * @template T = string | number | boolean | (string|number|boolean)[] | null
+     *
+     * @param {string} name
+     *
+     * @return {T}
+     *
+     * @throws {TypeError} If option does not exist
+     */
+    getOption<
+        T = string | number | boolean | (string|number|boolean)[] | null
+    >(name: string): T;
+
+    /**
+     * Determine if input option exists
+     *
+     * @param {string} name
+     *
+     * @return {boolean}
+     */
+    hasOption(name: string): boolean;
+    
+    /**
+     * Input options merged with their default values
+     * 
+     * @type {Map<string, string | number | boolean | (string | number | boolean)[] | null>}
+     */
+    get options(): Map<string, string | number | boolean | (string|number|boolean)[] | null>;
 }
