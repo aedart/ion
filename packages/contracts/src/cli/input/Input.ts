98d12475b903d583ddb7c56ccb2c10b6a689b4f5
alin <alin@rspsystems.com>
2025-04-07 10:15:40 +0200

    Add Input interface (incomplete)

---
+++ packages/contracts/src/cli/input/Input.ts
@@ -0,0 +1,12 @@
+/**
+ * Input
+ * 
+ * This component is an adaptation of Symfony Console's `InputInterface` - Copyright Fabien Potencier
+ * 2004-present, MIT License.
+ * 
+ * https://github.com/symfony/console/blob/7.1/Input/InputInterface.php
+ */
+export default interface Input
+{
+    // TODO: ...
+}
