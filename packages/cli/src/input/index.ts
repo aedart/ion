e5064c4677ea58ecd90403a6e6d1ef55577ed587
alin <alin@rspsystems.com>
2025-04-08 14:13:06 +0200

    Export MapInput

--- packages/cli/src/input/index.ts
+++ packages/cli/src/input/index.ts
@@ -2,10 +2,12 @@ import Argument from "./Argument";
 import Option from './Option';
 import Definition from './Definition';
 import BaseInput from './BaseInput';
+import MapInput from './MapInput';
 
 export {
     Argument,
     Option,
     Definition,
-    BaseInput
+    BaseInput,
+    MapInput
 }
