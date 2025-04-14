8bb192c145e09e95996ba983028f60f83216dd86
alin <alin@rspsystems.com>
2025-04-14 13:38:24 +0200

    Add Default Output Options

---
+++ packages/cli/src/output/DefaultOutputOptions.ts
@@ -0,0 +1,52 @@
+import type { OutputOptions } from "@aedart/contracts/cli";
+import { Mode, Verbosity } from "@aedart/contracts/cli";
+import { populate } from "@aedart/support/objects";
+
+/**
+ * Default Output Options
+ * 
+ * @see {import('@aedart/contracts/cli').OutputOptions}
+ */
+export default class DefaultOutputOptions implements OutputOptions
+{
+    /**
+     * Verbosity level
+     *
+     * @type {Verbosity}
+     */
+    verbosity: Verbosity = Verbosity.NORMAL;
+
+    /**
+     * Output mode
+     *
+     * @type {Mode}
+     */
+    mode: Mode = Mode.NORMAL;
+
+    /**
+     * Create new "default" Output Options instance
+     * 
+     * @param {OutputOptions} [options]
+     */
+    public constructor(options?: OutputOptions)
+    {
+        // Merge provided options, if any given
+        if (options && typeof options == 'object') {
+            populate(this, options);
+        }
+    }
+
+    /**
+     * Creates new Output Options instance from given options
+     * 
+     * @param {OutputOptions} [options]
+     * 
+     * @return {Readonly<DefaultOutputOptions | OutputOptions>}
+     */
+    public static from(options?: OutputOptions): Readonly<DefaultOutputOptions|OutputOptions>
+    {
+        const resolved = new this(options);
+
+        return Object.freeze(resolved);
+    }
+}
