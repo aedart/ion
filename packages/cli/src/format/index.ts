2734a3fdbf5d8ebcf13123f4352fcd0370e6e4fc
alin <alin@rspsystems.com>
2025-04-15 11:39:59 +0200

    Export BaseOutputFormatter

--- packages/cli/src/format/index.ts
+++ packages/cli/src/format/index.ts
@@ -1,9 +1,11 @@
 import NullStyle from "./NullStyle";
 import ChalkStyle from "./ChalkStyle";
 import NullOutputFormatter from "./NullOutputFormatter";
+import BaseOutputFormatter from "./BaseOutputFormatter";
 
 export {
     NullStyle,
     ChalkStyle,
-    NullOutputFormatter
+    NullOutputFormatter,
+    BaseOutputFormatter
 }
