eec9f6b2bc67135a2fa79aa8ea3913c6cb59fe54
alin <alin@rspsystems.com>
2025-04-15 10:12:18 +0200

    Export ChalkStyle

--- packages/cli/src/format/index.ts
+++ packages/cli/src/format/index.ts
@@ -1,7 +1,9 @@
 import NullStyle from "./NullStyle";
+import ChalkStyle from "./ChalkStyle";
 import NullOutputFormatter from "./NullOutputFormatter";
 
 export {
     NullStyle,
+    ChalkStyle,
     NullOutputFormatter
 }
