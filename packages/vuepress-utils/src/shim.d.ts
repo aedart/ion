439f7da709bcbba89f1000e732d7213de92c210e
aedart <aedart@gmail.com>
2026-04-05 16:51:11 +0200

    Fix style

--- packages/vuepress-utils/src/shim.d.ts
+++ packages/vuepress-utils/src/shim.d.ts
@@ -2,7 +2,8 @@
  * @see https://www.thisdot.co/blog/how-to-create-and-deploy-a-vue-component-library-to-npm
  * @see https://fettblog.eu/typescript-modules-for-webpack/
  */
-declare module '*.vue' {
+declare module '*.vue'
+{
     import type { DefineComponent } from 'vue';
     const component: DefineComponent<{}, {}, any>;
     export default component;
