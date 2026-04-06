7244edf5cb1eadc71ba287a2312084bf80992767
aedart <aedart@gmail.com>
2026-04-06 15:48:40 +0200

    Optimise performance of isPropertyKey()

--- packages/support/src/misc/isPropertyKey.ts
+++ packages/support/src/misc/isPropertyKey.ts
@@ -1,13 +1,15 @@
 /**
  * Determine if given key is a valid property key name
- * 
+ *
  * @see {PropertyKey}
- * 
+ *
  * @param {any} key
- * 
+ *
  * @returns {boolean} True if typeof key is a string, number or symbol
  */
 export function isPropertyKey(key: any): boolean
 {
-    return [ 'string', 'number', 'symbol' ].includes(typeof key);
+    const type = typeof key;
+
+    return type === 'string' || type === 'number' || type === 'symbol';
 }
