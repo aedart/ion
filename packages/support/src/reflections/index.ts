84d49a4e5974772d54c21af65376f8542fb784a2
aedart <aedart@gmail.com>
2026-04-07 18:36:09 +0200

    Port reflections classLooksLike()

--- packages/support/src/reflections/index.ts
+++ packages/support/src/reflections/index.ts
@@ -1,4 +1,5 @@
 export * from './assertHasPrototypeProperty.js';
+export * from './classLooksLike.js';
 export * from './classOwnKeys.js';
 export * from './getAllParentsOfClass.js';
 export * from './getParentOfClass.js';
