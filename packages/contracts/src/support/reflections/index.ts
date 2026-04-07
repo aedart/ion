e9065b3dcb021d741736ff104499c01b3c2a0aff
aedart <aedart@gmail.com>
2026-04-07 19:38:50 +0200

    Remove FUNCTION_PROTOTYPE
    No longer used by reflection utils

--- packages/contracts/src/support/reflections/index.ts
+++ packages/contracts/src/support/reflections/index.ts
@@ -6,17 +6,6 @@
 export const SUPPORT_REFLECTIONS: unique symbol = Symbol('@aedart/contracts/support/reflections');
 
 /**
- * The prototype of {@link Function}
- *
- * **Note**: _Prototype is obtained via `Reflect.getPrototypeOf(Function)`_
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/prototype
- *
- * @type {object}
- */
-export const FUNCTION_PROTOTYPE: object = Reflect.getPrototypeOf(Function) as object;
-
-/**
  * `TypedArray` prototype
  *
  * **Note**: _Prototype is obtained via `Reflect.getPrototypeOf(Int8Array)`_
