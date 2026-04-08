e5780077ff9f3d736817c56ae6f2744ce5629763
aedart <aedart@gmail.com>
2026-04-08 20:17:24 +0200

    Optimise performance of objects' isset()

--- packages/support/src/objects/isset.ts
+++ packages/support/src/objects/isset.ts
@@ -1,28 +1,30 @@
-import { isset as _isset } from "../misc/isset.js";
+import {isset as _isset} from "../misc/isset.js";
 import {get} from "./get.js";
 import type {Key} from "@aedart/contracts/support";
 
 /**
  * Determine if properties at given paths are declared, and their values are not undefined or null
- * 
+ *
  * @template T
- * 
+ *
  * @param {T} object
  * @param {...Key} paths
- * 
+ *
  * @returns {boolean}
  */
-export function isset<T>(object: T, ...paths: (Key)[]): boolean
+export function isset<T>(object: T, ...paths: Key[]): boolean
 {
-    if (object === undefined || paths.length === 0) {
+    const len = paths.length;
+    if (object === undefined || len === 0) {
         return false;
     }
 
-    for (const path of paths) {
-        if (!_isset(get(object, path))) {
+    // Performance: Use index-based loop for CPU cache locality and to avoid iterator overhead
+    for (let i = 0; i < len; i++) {
+        if (!_isset(get(object, paths[i]))) {
             return false;
         }
     }
-    
+
     return true;
 }
