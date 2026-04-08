52ae2e31ab50f76ef014027227ae6ecc1c0256c6
alin <alin@rspsystems.com>
2026-04-08 12:34:01 +0200

    Port tests for object set()

---
+++ tests/browser/support/objects/set.test.ts
@@ -0,0 +1,39 @@
+import { set, get, has } from '@aedart/support/objects'
+import { describe, expect, test } from 'vitest';
+
+describe('@aedart/support/objects', () => {
+    describe('set', () => {
+
+        test('can set property', function () {
+
+            const target = {};
+
+            const foo = Symbol('foo-symbol');
+            const values = [
+                { key: 'a', value: 1234 },
+                { key: 'b.name', value: 'Ole' },
+                { key: 'b.c', value: { age: 48 } },
+                { key: 'd[0]', value: { name: 'Tim' } },
+                { key: 'd[1].name', value: 'Erica' },
+                { key: foo, value: true },
+                { key: ['e', 'nested', foo], value: 'bar' },
+                { key: ['e', foo, 3], value: 'zim' },
+            ];
+
+            values.forEach(({key, value}, index) => {
+                set(target, key, value);
+
+                expect(has(target, key), `Target does not contain key for values index ${index}`)
+                    .toBeTruthy()
+
+                expect(get(target, key), `Incorrect value in target for values index ${index}`)
+                    .toBe(value);
+            });
+
+            // Debug
+            // console.log(target);
+            // console.log(target.d);
+            // console.log(target.e);
+        });
+    });
+});
