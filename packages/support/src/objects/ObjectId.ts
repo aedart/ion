1fde820ba9e7a69f169483b1e029b33b7de5c2c3
alin <alin@rspsystems.com>
2026-04-08 12:50:24 +0200

    Refactor, make properties private
    Also minor optimisation of get()

--- packages/support/src/objects/ObjectId.ts
+++ packages/support/src/objects/ObjectId.ts
@@ -1,6 +1,6 @@
 /**
  * Object ID
- * 
+ *
  * Utility that is able to return a numeric ID for objects.
  * 
  * Source is heavily inspired by Nicolas Gehlert's blog post:
@@ -13,56 +13,47 @@ export default class ObjectId
 {
     /**
      * Internal counter
-     * 
-     * @type {number}
-     * 
-     * @protected
+     *
      * @static
      */
-    protected static _count: number = 0;
+    static #count: number = 0;
 
     /**
      * Weak Map of objects and their associated id
-     * 
-     * @type {WeakMap<object, number>}
-     * 
-     * @protected
-     * @readonly
+     *
+     * @static
      */
-    protected static _map: WeakMap<object, number> = new WeakMap<object, number>();
+    static readonly #map: WeakMap<object, number> = new WeakMap<object, number>();
 
     /**
      * Returns a unique ID for target object.
-     * 
-     * If no ID exists for the given object, then a new ID is
-     * generated and returned. Subsequent calls to this method using
-     * the same object will return the same ID.
-     * 
+     *
      * @param {object} target
+     *
      * @returns {number}
      */
     static get(target: object): number
     {
-        const id: number | undefined = ObjectId._map.get(target);
+        const id = ObjectId.#map.get(target);
         if (id !== undefined) {
             return id;
         }
 
-        ObjectId._count += 1;
-        ObjectId._map.set(target, ObjectId._count);
-        
-        return ObjectId._count;
+        const newId = ++ObjectId.#count;
+        ObjectId.#map.set(target, newId);
+
+        return newId;
     }
 
     /**
      * Determine if a unique ID exists for target object
-     * 
+     *
      * @param {object} target
-     * 
+     *
      * @returns {boolean}
      */
     static has(target: object): boolean
     {
-        return ObjectId._map.has(target);
+        return ObjectId.#map.has(target);
     }
 }
