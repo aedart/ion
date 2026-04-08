6bf8301d37de87d7fd482263c7b37d24ea30b084
aedart <aedart@gmail.com>
2026-04-08 20:02:27 +0200

    Port and refactor Populatable interface
    Also, changed the type of data attribute.

---
+++ packages/contracts/src/support/objects/Populatable.ts
@@ -0,0 +1,22 @@
+/**
+ * Populatable
+ * 
+ * Able to be populated (hydrated) with data
+ */
+export default interface Populatable
+{
+    /**
+     * Populate this component with data
+     *
+     * **Note**: _When no `data` is provided, then nothing is populated_
+     * 
+     * @param {Record<PropertyKey, any>} [data]
+     * 
+     * @returns {this}
+     * 
+     * @throws {TypeError} When unable to populate with given data
+     */
+    populate(
+        data?: Record<PropertyKey, any> /* eslint-disable-line @typescript-eslint/no-explicit-any */
+    ): this
+}
--- packages/contracts/src/support/objects/index.ts
+++ packages/contracts/src/support/objects/index.ts
@@ -6,8 +6,10 @@
 export const SUPPORT_OBJECTS: unique symbol = Symbol('@aedart/contracts/support/objects');
 
 import Cloneable from "./Cloneable.js";
+import Populatable from "./Populatable.js";
 export {
     type Cloneable,
+    type Populatable,
 }
 
 export { CLONE } from "./Cloneable.js";
