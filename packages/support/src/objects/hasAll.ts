ed1ec9fc4125f817cfb1563b1788ede600560825
alin <alin@rspsystems.com>
2026-04-08 11:03:16 +0200

    Optimise performance of hasAll()

--- packages/support/src/objects/hasAll.ts
+++ packages/support/src/objects/hasAll.ts
@@ -1,23 +1,24 @@
-import { has } from './has.js';
+import {has} from './has.js';
 import type {Key} from "@aedart/contracts/support";
 
 /**
  * Determine if all paths are properties of given object
+ *
  * @template T
- * 
+ *
  * @param {T} object Target object
  * @param {...Key} paths Property path(s)
- * 
+ *
  * @returns {boolean}
  */
-export function hasAll<T>(object: T, ...paths: (Key)[]): boolean
+export function hasAll<T>(object: T, ...paths: Key[]): boolean
 {
     if (object === undefined || paths.length === 0) {
         return false;
     }
 
-    for (const path of paths) {
-        if (!has(object, path)) {
+    for (let i = 0, len = paths.length; i < len; i++) {
+        if (!has(object, paths[i])) {
             return false;
         }
     }
