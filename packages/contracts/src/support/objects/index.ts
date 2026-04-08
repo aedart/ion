5575f3036215edce909fffff3860b74597c97505
aedart <aedart@gmail.com>
2026-04-08 19:07:58 +0200

    Port Cloneable interface

---
+++ packages/contracts/src/support/objects/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Contracts Support Objects identifier
+ *
+ * @type {Symbol}
+ */
+export const SUPPORT_OBJECTS: unique symbol = Symbol('@aedart/contracts/support/objects');
+
+import Cloneable from "./Cloneable.js";
+export {
+    type Cloneable,
+}
