3786e2187688169cb2e1708ab7e76e2e53777bf8
aedart <aedart@gmail.com>
2026-04-08 19:16:33 +0200

    Redesign interface to use unique symbol as method name

--- packages/contracts/src/support/objects/Cloneable.ts
+++ packages/contracts/src/support/objects/Cloneable.ts
@@ -1,14 +1,21 @@
 /**
+ * Cloneable Symbol
+ *
+ * @type {unique symbol}
+ */
+export const CLONE: unique symbol = Symbol('@aedart/support/objects/clone');
+
+/**
  * Cloneable
  */
 export default interface Cloneable
 {
     /**
      * Returns a clone (new instance) of this object
-     * 
+     *
      * @return {this}
-     * 
+     *
      * @throws {Error}
      */
-    clone(): this;
+    [CLONE](): this;
 }
