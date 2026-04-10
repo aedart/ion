f7109b4049ced0507d52a3980fbd2a8ef90e09b8
aedart <aedart@gmail.com>
2026-04-10 16:33:51 +0200

    Optimise performance of getNameOrDesc()
    The descTag() is now only invoked if the getConstructorName() returns null. This is slightly faster than before.

--- packages/support/src/reflections/getNameOrDesc.ts
+++ packages/support/src/reflections/getNameOrDesc.ts
@@ -1,23 +1,24 @@
-import type { ConstructorLike } from "@aedart/contracts";
-import { descTag } from "../misc/descTag.js";
-import { getConstructorName } from "./getConstructorName.js";
+import type {ConstructorLike} from "@aedart/contracts";
+import {descTag} from "../misc/descTag.js";
+import {getConstructorName} from "./getConstructorName.js";
 
 /**
- * Return target class' constructor name or default to target's description tag if a name is unavailable 
- * 
- * **Note**: _Method is a shortcut for the following:_
- * ```js
- * getConstructorName(target, descTag(target));
- * ```
- * 
+ * Return target class' constructor name or default to target's description tag if unavailable
+ *
  * @see getConstructorName
  * @see descTag
- * 
+ *
  * @param {ConstructorLike} target
- * 
+ *
  * @return {string}
  */
 export function getNameOrDesc(target: ConstructorLike): string
 {
-    return getConstructorName(target, descTag(target)) as string;
+    const name: string | null = getConstructorName(target);
+
+    if (name !== null) {
+        return name;
+    }
+
+    return descTag(target);
 }
