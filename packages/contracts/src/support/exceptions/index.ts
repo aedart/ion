1772fc69ad8b45714665a6d5c2ceceadbe69b8eb
aedart <aedart@gmail.com>
2026-04-10 16:08:03 +0200

    Port Throwable interface

---
+++ packages/contracts/src/support/exceptions/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Contracts Support Exceptions identifier
+ *
+ * @type {Symbol}
+ */
+export const SUPPORT_EXCEPTIONS: unique symbol = Symbol('@aedart/contracts/support/exceptions');
+
+import Throwable from "./Throwable.js";
+export {
+    type Throwable
+}
