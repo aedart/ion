2d069b446f7357f1d9f46782be49a69b7fc4ef3d
aedart <aedart@gmail.com>
2026-04-11 19:38:03 +0200

    Fix incorrect TYPED_ARRAY_PROTOTYPE

--- packages/contracts/src/support/reflections/index.ts
+++ packages/contracts/src/support/reflections/index.ts
@@ -6,15 +6,13 @@
 export const SUPPORT_REFLECTIONS: unique symbol = Symbol('@aedart/contracts/support/reflections');
 
 /**
- * `TypedArray` prototype
+ * `TypedArray` instance prototype
  *
- * **Note**: _Prototype is obtained via `Reflect.getPrototypeOf(Int8Array)`_
- *
+ * **Note**: _Prototype is obtained via `Reflect.getPrototypeOf(Int8Array.prototype)`_
+ * 
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray
- *
- * @type {object}
  */
-export const TYPED_ARRAY_PROTOTYPE: object = Reflect.getPrototypeOf(Int8Array) as object;
+export const TYPED_ARRAY_PROTOTYPE: object = Reflect.getPrototypeOf(Int8Array.prototype) as object;
 
 import ClassBlueprint from './ClassBlueprint.js';
 export { type ClassBlueprint };
