67dfacddb3b2121aee998fd4856d740d43330a51
aedart <aedart@gmail.com>
2026-04-11 19:38:54 +0200

    Fix and optimise isTypedArray()

--- packages/support/src/arrays/isTypedArray.ts
+++ packages/support/src/arrays/isTypedArray.ts
@@ -1,16 +1,22 @@
-import { TYPED_ARRAY_PROTOTYPE } from "@aedart/contracts/support/reflections";
-import { Constructor } from "@aedart/contracts";
+import {TYPED_ARRAY_PROTOTYPE} from "@aedart/contracts/support/reflections";
 
 /**
  * Determine if given target is an instance of a `TypedArray`
- * 
+ *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray
- * 
- * @param {object} target
- * 
+ *
+ * @param {object|null} target
+ *
  * @return {boolean}
  */
-export function isTypedArray(target: object): boolean
+export function isTypedArray(target: object | null): boolean
 {
-    return target instanceof (TYPED_ARRAY_PROTOTYPE as Constructor);
+    // Fast-exit for null or non-objects
+    if (target === null || typeof target !== 'object') {
+        return false;
+    }
+
+    // Use isPrototypeOf because TYPED_ARRAY_PROTOTYPE is the prototype object,
+    // not the constructor function.
+    return (TYPED_ARRAY_PROTOTYPE as object).isPrototypeOf(target);
 }
