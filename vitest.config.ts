ae23fa721c4177b81f675db4d733582cc88f7309
aedart <aedart@gmail.com>
2026-04-20 20:12:47 +0200

    Switch to using @rolldown/plugin-babel
    Finally, this seemed to work as intended.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vitest/config';
-import swc from 'unplugin-swc';
+// import swc from 'unplugin-swc';
+import babelPlugin from "@rolldown/plugin-babel";
 
 export default defineConfig({
     test: {
@@ -24,23 +25,42 @@ export default defineConfig({
             },
             {
                 // Disable default transformers to prevent them from skipping the @ symbol
-                oxc: false,
-                esbuild: false,
-
+                // oxc: false,
+                // esbuild: false,
+                
                 plugins: [
-                    swc.vite({
-                        jsc: {
-                            parser: {
-                                syntax: 'typescript',
-                                decorators: true,
-                            },
-                            transform: {
-                                decoratorVersion: '2023-11',
+                    
+                    // WARNING: Do NOT use this for decorator transpiling. It has WRONG context.metadata
+                    // inheritance.
+                    // swc.vite({
+                    //     jsc: {
+                    //         parser: {
+                    //             syntax: 'typescript',
+                    //             decorators: true,
+                    //         },
+                    //         transform: {
+                    //             decoratorVersion: '2023-11',
+                    //
+                    //             // This corresponds to TS `emitDecoratorMetadata` (which is outdated)
+                    //             decoratorMetadata: false,
+                    //         },
+                    //         target: "es2024"
+                    //     },
+                    // }),
 
-                                // This corresponds to TS `emitDecoratorMetadata` (which is outdated)
-                                decoratorMetadata: false,
+                    babelPlugin({
+                        presets: [{
+                            preset: () => ({
+                                plugins: [
+                                    ["@babel/plugin-proposal-decorators", {version: "2023-11"}]
+                                ]
+                            }),
+                            rolldown: {
+                                filter: {
+                                    code: "@"
+                                }
                             },
-                        },
+                        }],
                     }),
                 ],
 
