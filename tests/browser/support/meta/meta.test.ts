eecafc26701e36f2af23c15bb2ad3c8fcc64e596
alin <alin@rspsystems.com>
2026-05-11 16:36:33 +0200

    Add test for storing symbol on static method

--- tests/browser/support/meta/meta.test.ts
+++ tests/browser/support/meta/meta.test.ts
@@ -246,6 +246,22 @@ describe('@meta() decorator', () => {
         expect(Metadata.get(MyClass, ['methods', 'myMethod', MY_KEY])).toBe('secret-value');
     });
 
+    test('can use Symbols as metadata keys, for static method', () => {
+        const MY_KEY = Symbol('my_key');
+
+        class MyClass
+        {
+            @meta(MY_KEY, 'secret-value')
+            static myMethod()
+            {/* empty */}
+        }
+
+        // Debug
+        // new MyClass();
+
+        expect(Metadata.get(MyClass, ['static', 'methods', 'myMethod', MY_KEY])).toBe('secret-value');
+    });
+    
     describe('@meta() security: prototype pollution', () => {
         test('immediately prevents pollution via class decorator', () => {
             const trigger = () => {
