2e97716a231aa4bfca5175dbe15f0767401ac3b2
aedart <aedart@gmail.com>
2026-05-13 20:07:01 +0200

    WIP

--- tests/browser/support/meta/meta.test.ts
+++ tests/browser/support/meta/meta.test.ts
@@ -264,6 +264,31 @@ describe('@meta() decorator', () => {
         );
     });
 
+    test('can get meta using instance method reference', () => {
+        class MyService
+        {
+            @meta('foo', 'bar')
+            play()
+            {/* empty */}
+        }
+
+        // Debug
+        const instance = new MyService();
+        const { play } = instance; // eslint-disable-line @typescript-eslint/unbound-method
+
+        expect(Metadata.has(play, 'foo'), 'Unable to determine if has meta for instance method')
+            .toBe(true);
+
+        expect(Metadata.get(play, 'foo'), 'Incorrect meta value for instance method')
+            .toBe('bar');
+    });
+
+    // TODO: Inherit instance method meta, ... obtain via member directly.
+    // TODO: getter / setter (field) meta, ... obtain via member directly.
+    // TODO: Static method meta, ... obtain via static member directly.
+    // TODO: Inherit static method meta, ... obtain via static member directly.
+    // TODO: Static getter / setter (field) meta, ... obtain via member directly.
+    
     describe('@meta() security: prototype pollution', () => {
         test('immediately prevents pollution via class decorator', () => {
             const trigger = () => {
