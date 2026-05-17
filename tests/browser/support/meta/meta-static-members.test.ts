ad962f28911bafacd795298560204e02034db709
aedart <aedart@gmail.com>
2026-05-17 14:18:59 +0200

    Add tests for overridden static members and use of @inheritMeta()

--- tests/browser/support/meta/meta-static-members.test.ts
+++ tests/browser/support/meta/meta-static-members.test.ts
@@ -1,4 +1,4 @@
-import { meta, Metadata } from '@aedart/support/meta';
+import { meta, Metadata, inheritMeta } from '@aedart/support/meta';
 import { getClassPropertyDescriptors } from '@aedart/support/reflections';
 import { describe, expect, test } from 'vitest';
 
@@ -34,29 +34,40 @@ describe('@meta() decorator (static members)', () => {
     class BetaService extends AlphaService
     {}
 
-    // TODO: WIP
-    // class GammaService extends BetaService
-    // {
-    //     // Note: meta is sadly NOT automatically inherited for overridden static members.
-    //     // This is because decorator's context.addInitializer() do not offer a late static
-    //     // binding for `this` (this = BaseService in this case). For this reason, the only
-    //     // way to ensure inheritance, is via the custom `@inheritMeta()`!
-    //
-    //     static override myField = 'cda';
-    //
-    //     static override accessor value = 51;
-    //
-    //     static override get name(): string {
-    //         return this._name;
-    //     }
-    //
-    //     static override set name(n: string) {
-    //         this._name = n;
-    //     }
-    //
-    //     static override play()
-    //     {/* empty */}
-    // }
+    class GammaService extends BetaService
+    {
+        // Note: meta is sadly NOT automatically inherited for overridden static members.
+        // This is because decorator's context.addInitializer() do not offer a late static
+        // binding for `this` (this = BaseService in this case). For this reason, the only
+        // way to obtain inherited metadata via a static member reference, is via the 
+        // @inheritMeta() decorator.
+        
+        @inheritMeta() // NOTE: Has NO effect for field type, because "target" is undefined in decorator!
+        static override myField = 'cda';
+
+        @inheritMeta()
+        static override accessor value = 51;
+
+        @inheritMeta()
+        static override get name(): string {
+            return this._name;
+        }
+
+        @inheritMeta()
+        static override set name(n: string) {
+            this._name = n;
+        }
+
+        @inheritMeta()
+        static override play()
+        {/* empty */}
+    }
+    
+    class SierraService extends GammaService
+    {}
+    
+    class TangoService extends SierraService
+    {}
 
     test('can get meta using static member reference', () => {
         // --------------------------------------------------------------------------------------------------- //
@@ -219,84 +230,163 @@ describe('@meta() decorator (static members)', () => {
             .toBe('bar');
     });
 
-    // TODO: WIP
-    // test('inherits meta using overridden static member reference (via @inheritMeta decorator)', () => {
-    //
-    //     // --------------------------------------------------------------------------------------------------- //
-    //     // Field: Same as previous tests...
-    //     expect(Metadata.get(GammaService, 'static.fields.myField.wip'), 'Unable to get meta for field')
-    //         .toBe('wap');
-    //
-    //     // --------------------------------------------------------------------------------------------------- //
-    //     // Get the class descriptors, just like in the previous test
-    //     const overriddenDescriptors = getClassPropertyDescriptors(
-    //         GammaService,
-    //         true,
-    //         false
-    //     );
-    //
-    //     // Debug
-    //     console.log('descriptors (overridden)', overriddenDescriptors);
-    //
-    //     // --------------------------------------------------------------------------------------------------- //
-    //     // Accessor
-    //
-    //     const valueDescriptor = overriddenDescriptors.value;
-    //
-    //     expect(
-    //         Metadata.has(valueDescriptor.get as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
-    //         'Unable to determine if has meta for accessor (get)',
-    //     )
-    //         .toBe(true);
-    //     expect(
-    //         Metadata.get(valueDescriptor.get as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
-    //         'Incorrect meta value for accessor (get)',
-    //     )
-    //         .toBe('fup');
-    //     expect(
-    //         Metadata.has(valueDescriptor.set as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
-    //         'Unable to determine if has meta for accessor (set)',
-    //     )
-    //         .toBe(true);
-    //     expect(
-    //         Metadata.get(valueDescriptor.set as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
-    //         'Incorrect meta value for accessor (set)',
-    //     )
-    //         .toBe('fup');
-    //
-    //     // --------------------------------------------------------------------------------------------------- //
-    //     // Getter / Setter
-    //
-    //     const nameDescriptor = overriddenDescriptors.name;
-    //
-    //     expect(
-    //         Metadata.has(nameDescriptor.get as object, 'get_name'), // eslint-disable-line @typescript-eslint/unbound-method
-    //         'Unable to determine if has meta for getter',
-    //     )
-    //         .toBe(true);
-    //     expect(
-    //         Metadata.get(nameDescriptor.get as object, 'get_name'), // eslint-disable-line @typescript-eslint/unbound-method
-    //         'Incorrect meta value for getter',
-    //     )
-    //         .toBe('zar');
-    //     expect(
-    //         Metadata.has(nameDescriptor.get as object, 'set_name'), // eslint-disable-line @typescript-eslint/unbound-method
-    //         'Unable to determine if has meta for setter',
-    //     )
-    //         .toBe(true);
-    //     expect(
-    //         Metadata.get(nameDescriptor.get as object, 'set_name'), // eslint-disable-line @typescript-eslint/unbound-method
-    //         'Incorrect meta value for setter',
-    //     )
-    //         .toBe('fin');
-    //
-    //     // --------------------------------------------------------------------------------------------------- //
-    //     // Method: This should be straight forward...
-    //
-    //     const { play } = GammaService; // eslint-disable-line @typescript-eslint/unbound-method
-    //     expect(Metadata.has(play, 'foo'), 'Unable to determine if has meta for instance method')
-    //         .toBe(true);
-    //     expect(Metadata.get(play, 'foo'), 'Incorrect meta value for instance method')
-    //         .toBe('bar');
-    // });
+    test('inherits meta using overridden static member reference (via @inheritMeta())', () => {
+
+        // --------------------------------------------------------------------------------------------------- //
+        // Field: Same as previous tests...
+        expect(Metadata.get(GammaService, 'static.fields.myField.wip'), 'Unable to get meta for field')
+            .toBe('wap');
+
+        // --------------------------------------------------------------------------------------------------- //
+        // Get the class descriptors, just like in the previous test
+        const overriddenDescriptors = getClassPropertyDescriptors(
+            GammaService,
+            true,
+            false
+        );
+
+        // Debug
+        // console.log('descriptors (overridden)', overriddenDescriptors);
+
+        // --------------------------------------------------------------------------------------------------- //
+        // Accessor
+
+        const valueDescriptor = overriddenDescriptors.value;
+
+        expect(
+            Metadata.has(valueDescriptor.get as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Unable to determine if has meta for accessor (get)',
+        )
+            .toBe(true);
+        expect(
+            Metadata.get(valueDescriptor.get as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Incorrect meta value for accessor (get)',
+        )
+            .toBe('fup');
+        expect(
+            Metadata.has(valueDescriptor.set as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Unable to determine if has meta for accessor (set)',
+        )
+            .toBe(true);
+        expect(
+            Metadata.get(valueDescriptor.set as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Incorrect meta value for accessor (set)',
+        )
+            .toBe('fup');
+
+        // --------------------------------------------------------------------------------------------------- //
+        // Getter / Setter
+
+        const nameDescriptor = overriddenDescriptors.name;
+
+        expect(
+            Metadata.has(nameDescriptor.get as object, 'get_name'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Unable to determine if has meta for getter',
+        )
+            .toBe(true);
+        expect(
+            Metadata.get(nameDescriptor.get as object, 'get_name'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Incorrect meta value for getter',
+        )
+            .toBe('zar');
+        expect(
+            Metadata.has(nameDescriptor.get as object, 'set_name'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Unable to determine if has meta for setter',
+        )
+            .toBe(true);
+        expect(
+            Metadata.get(nameDescriptor.get as object, 'set_name'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Incorrect meta value for setter',
+        )
+            .toBe('fin');
+
+        // --------------------------------------------------------------------------------------------------- //
+        // Method: This should be straight forward...
+
+        const { play } = GammaService; // eslint-disable-line @typescript-eslint/unbound-method
+        expect(Metadata.has(play, 'foo'), 'Unable to determine if has meta for instance method')
+            .toBe(true);
+        expect(Metadata.get(play, 'foo'), 'Incorrect meta value for instance method')
+            .toBe('bar');
+    });
+
+    test('inherits meta using overridden static member reference (deep inheritance chain)', () => {
+
+        // --------------------------------------------------------------------------------------------------- //
+        // Field: Same as previous tests...
+        expect(Metadata.get(TangoService, 'static.fields.myField.wip'), 'Unable to get meta for field')
+            .toBe('wap');
+
+        // --------------------------------------------------------------------------------------------------- //
+        // Get the class descriptors, just like in the previous test
+        const overriddenDescriptors = getClassPropertyDescriptors(
+            TangoService,
+            true,
+            false
+        );
+
+        // Debug
+        // console.log('descriptors (overridden)', overriddenDescriptors);
+
+        // --------------------------------------------------------------------------------------------------- //
+        // Accessor
+
+        const valueDescriptor = overriddenDescriptors.value;
+
+        expect(
+            Metadata.has(valueDescriptor.get as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Unable to determine if has meta for accessor (get)',
+        )
+            .toBe(true);
+        expect(
+            Metadata.get(valueDescriptor.get as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Incorrect meta value for accessor (get)',
+        )
+            .toBe('fup');
+        expect(
+            Metadata.has(valueDescriptor.set as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Unable to determine if has meta for accessor (set)',
+        )
+            .toBe(true);
+        expect(
+            Metadata.get(valueDescriptor.set as object, 'fip'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Incorrect meta value for accessor (set)',
+        )
+            .toBe('fup');
+
+        // --------------------------------------------------------------------------------------------------- //
+        // Getter / Setter
+
+        const nameDescriptor = overriddenDescriptors.name;
+
+        expect(
+            Metadata.has(nameDescriptor.get as object, 'get_name'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Unable to determine if has meta for getter',
+        )
+            .toBe(true);
+        expect(
+            Metadata.get(nameDescriptor.get as object, 'get_name'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Incorrect meta value for getter',
+        )
+            .toBe('zar');
+        expect(
+            Metadata.has(nameDescriptor.get as object, 'set_name'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Unable to determine if has meta for setter',
+        )
+            .toBe(true);
+        expect(
+            Metadata.get(nameDescriptor.get as object, 'set_name'), // eslint-disable-line @typescript-eslint/unbound-method
+            'Incorrect meta value for setter',
+        )
+            .toBe('fin');
+
+        // --------------------------------------------------------------------------------------------------- //
+        // Method: This should be straight forward...
+
+        const { play } = TangoService; // eslint-disable-line @typescript-eslint/unbound-method
+        expect(Metadata.has(play, 'foo'), 'Unable to determine if has meta for instance method')
+            .toBe(true);
+        expect(Metadata.get(play, 'foo'), 'Incorrect meta value for instance method')
+            .toBe('bar');
+    });
 });
