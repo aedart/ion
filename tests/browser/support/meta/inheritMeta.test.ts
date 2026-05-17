5b6c652bbcc9e9bdd622ac4423f27eeb35415d45
aedart <aedart@gmail.com>
2026-05-17 14:16:30 +0200

    Add "fail" tests for inheritMeta decorator
    Remaining tests are located in the "meta static members" tests.

---
+++ tests/browser/support/meta/inheritMeta.test.ts
@@ -0,0 +1,32 @@
+import { inheritMeta } from '@aedart/support/meta';
+import { describe, expect, test } from 'vitest';
+
+describe('@inheritMeta() decorator', () => {
+    test('fails when used on class', () => {
+        const trigger = () => {
+            
+            // Not intended to be used on a class...
+            @inheritMeta()
+            class A
+            {}
+        }
+
+        expect(trigger).toThrow();
+    });
+    
+    test('fails when used on non-static members', () => {
+        const trigger = () => {
+            class A
+            {
+                // Not intended to be used on non-static member
+                @inheritMeta()
+                foo()
+                { /* empty */ }
+            }
+        }
+
+        expect(trigger).toThrow();
+    });
+    
+    // NOTE: See additional tests for @inheritMeta() in `meta-static-members.test.ts`!
+});
